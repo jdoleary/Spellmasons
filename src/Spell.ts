08146d0f56fa2bd0591e8f1d249ac4170a59d16d
Jordan O'Leary <jdoleary@gmail.com>
2021-02-23 08:15:48 -0500

    src: Allow aoe to be cast on empty space

--- src/Spell.ts
+++ src/Spell.ts
@@ -98,60 +98,58 @@ export function effect(spell: Spell, args: EffectArgs) {
     spell.delay--;
     return;
   }
-  if (unit) {
-    if (ignore.includes(unit)) {
-      return;
-    }
-    if (spell.damage) {
-      floatingText({
-        cellX: unit.x,
-        cellY: unit.y,
-        text: toString(spell),
-        color: 'red',
-      });
-      unit.takeDamage(spell.damage, 'spell');
-    }
-    if (spell.freeze) {
-      unit.frozen = true;
-    }
-    if (game) {
-      if (spell.aoe_radius) {
-        const withinRadius = game.getUnitsWithinDistanceOfPoint(
-          unit.x,
-          unit.y,
-          1,
-        );
-        for (let unit_in_radius of withinRadius) {
-          // If not self (because self has already been cast on)
-          if (unit_in_radius !== unit) {
-            // Cast on units in radius but turn off aoe_radius
-            // so it doesn't recur
-            effect(
-              { ...spell, aoe_radius: 0 },
-              {
-                ...args,
-                unit: unit_in_radius,
-              },
-            );
-          }
-        }
-      }
-      if (spell.chain) {
-        const chained_units = game.getTouchingUnitsRecursive(unit.x, unit.y, 1);
-        for (let chained_unit of chained_units) {
-          if (chained_unit === unit) {
-            // Skip current unit who has already taken damage
-            continue;
-          }
-          // Cast on each chained unit without chaining again
+  if (unit && ignore.includes(unit)) {
+    return;
+  }
+  if (unit && spell.damage) {
+    floatingText({
+      cellX: unit.x,
+      cellY: unit.y,
+      text: toString(spell),
+      color: 'red',
+    });
+    unit.takeDamage(spell.damage, 'spell');
+  }
+  if (unit && spell.freeze) {
+    unit.frozen = true;
+  }
+  if (game) {
+    if (spell.aoe_radius) {
+      const withinRadius = game.getUnitsWithinDistanceOfPoint(
+        spell.x,
+        spell.y,
+        1,
+      );
+      for (let unit_in_radius of withinRadius) {
+        // If not self (because self has already been cast on)
+        if (unit_in_radius !== unit) {
+          // Cast on units in radius but turn off aoe_radius
+          // so it doesn't recur
           effect(
-            { ...spell, chain: false },
+            { ...spell, aoe_radius: 0 },
             {
               ...args,
-              unit: chained_unit,
+              unit: unit_in_radius,
             },
           );
         }
+      }
+    }
+    if (unit && spell.chain) {
+      const chained_units = game.getTouchingUnitsRecursive(unit.x, unit.y, 1);
+      for (let chained_unit of chained_units) {
+        if (chained_unit === unit) {
+          // Skip current unit who has already taken damage
+          continue;
+        }
+        // Cast on each chained unit without chaining again
+        effect(
+          { ...spell, chain: false },
+          {
+            ...args,
+            unit: chained_unit,
+          },
+        );
       }
     }
   }
