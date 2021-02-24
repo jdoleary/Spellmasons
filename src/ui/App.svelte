d06ab9cdba69e0ea332344580ca5814c5fdafc0f
Jordan O'Leary <jdoleary@gmail.com>
2021-02-24 08:38:35 -0500

    src: Start to rebuild SpellBuilder imperatively

--- src/ui/App.svelte
+++ src/ui/App.svelte
@@ -8,24 +8,5 @@
     currentSpell = spell;
   });
   function clickOnBoard(e) {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const cell_x = Math.floor(x / CELL_SIZE);
-    const cell_y = Math.floor(y / CELL_SIZE);
-    console.log('Click in cell:', cell_x, cell_y, currentSpell);
-    if(currentSpell){
-      const {spell_type,...spell} = currentSpell;
-      if(spell_type === 'summon'){
-        const vy = cell_y > 3 ? -1 : 1
-        window.pie.sendData({ type: MESSAGE_TYPES.SPELL, spell:{
-          x: cell_x,
-          y: cell_y,
-          summon: { vx:0, vy, imagePath: 'crocodile.png' },
-        }});
-      }else{
-        window.pie.sendData({ type: MESSAGE_TYPES.SPELL, spell:{...spell, x:cell_x, y:cell_y} });
-      }
-    }
   }
 </script>
