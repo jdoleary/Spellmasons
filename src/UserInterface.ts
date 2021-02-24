9dfa2babddab59f3fd3fc2a8095c58191e5f1693
Jordan O'Leary <jdoleary@gmail.com>
2021-02-24 08:18:54 -0500

    src: Add reset game button
    bug: Fix click handler for endTurn button

--- src/UserInterface.ts
+++ src/UserInterface.ts
@@ -1,7 +1,12 @@
 import { MESSAGE_TYPES } from './index';
+import Game, { game_state } from './Game';
+
 const elControls = document.getElementById('controls');
 const elEndTurnBtn: HTMLButtonElement = document.getElementById(
   'endTurn',
+) as HTMLButtonElement;
+const elResetGameButton: HTMLButtonElement = document.getElementById(
+  'resetGame',
 ) as HTMLButtonElement;
 const elMana = document.getElementById('mana');
 
@@ -9,12 +14,26 @@ export function setup() {
   window.addEventListener('keydown', (event) => {
     switch (event.code) {
       case 'Space':
-        window.pie.sendData({ type: MESSAGE_TYPES.END_TURN });
+        endTurn();
         break;
       default:
         console.log('No set action for key' + event.code);
     }
   });
+  elEndTurnBtn.addEventListener('click', endTurn);
+  elResetGameButton.addEventListener('click', resetGame);
+}
+function endTurn() {
+  window.pie.sendData({ type: MESSAGE_TYPES.END_TURN });
+}
+function resetGame() {
+  const doReset = window.confirm('Are you sure you want to start over?');
+  if (doReset) {
+    window.pie.sendData({
+      type: MESSAGE_TYPES.LOAD_GAME_STATE,
+      game: new Game(),
+    });
+  }
 }
 export function turnEnded(isEnded: boolean) {
   elEndTurnBtn.disabled = isEnded;
