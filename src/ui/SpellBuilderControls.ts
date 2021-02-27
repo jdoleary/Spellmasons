2d240d8ecb25fc070eb00d3338d9f282a665c6b5
Jordan O'Leary <jdoleary@gmail.com>
2021-02-26 17:34:07 -0800

    ref: Refactor board click listener
    Allow pointer events for children of board so that
    we can trigger behavior when an image is hovered for example.
    Changing target to elBoard in the click listener will ensure that
    the proper clicked cell is calcualted regardless of the target of the
    click.

--- src/ui/SpellBuilderControls.ts
+++ src/ui/SpellBuilderControls.ts
@@ -9,50 +9,46 @@ const elBoard = document.getElementById('board');
 export default function setupSpellBuilderUI() {
   // Add board click handling
   elBoard.addEventListener('click', (e) => {
-    const target = e.target as HTMLElement;
-    if (e.target === elBoard) {
-      const rect = target.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-      let cell_x = Math.floor(x / CELL_SIZE);
-      let cell_y = Math.floor(y / CELL_SIZE);
-      if (window.inverted) {
-        cell_x = Math.abs((cell_x + 1 - BOARD_WIDTH) % BOARD_WIDTH);
-        cell_y = Math.abs((cell_y + 1 - BOARD_HEIGHT) % BOARD_HEIGHT);
-      }
-      console.log('Click in cell:', cell_x, cell_y, currentSpell);
-      if (currentSpell) {
-        if (currentSpell.summon) {
-          const vy = cell_y > 3 ? -1 : 1;
-          if (
-            window.game.units.filter((u) => u.alive && u.x === cell_x && u.y === cell_y)
-              .length
-          ) {
-            floatingText({
-              cellX: cell_x,
-              cellY: cell_y,
-              color: 'red',
-              text: "Cannot cast here, something's in the way",
-            });
-          } else {
-            window.pie.sendData({
-              type: MESSAGE_TYPES.SPELL,
-              spell: {
-                x: cell_x,
-                y: cell_y,
-                summon: { ...currentSpell.summon, vx: 0, vy },
-              },
-            });
-          }
+    const rect = elBoard.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    let cell_x = Math.floor(x / CELL_SIZE);
+    let cell_y = Math.floor(y / CELL_SIZE);
+    if (window.inverted) {
+      cell_x = Math.abs((cell_x + 1 - BOARD_WIDTH) % BOARD_WIDTH);
+      cell_y = Math.abs((cell_y + 1 - BOARD_HEIGHT) % BOARD_HEIGHT);
+    }
+    console.log('Click in cell:', cell_x, cell_y, currentSpell);
+    if (currentSpell) {
+      if (currentSpell.summon) {
+        const vy = cell_y > 3 ? -1 : 1;
+        if (
+          window.game.units.filter(
+            (u) => u.alive && u.x === cell_x && u.y === cell_y,
+          ).length
+        ) {
+          floatingText({
+            cellX: cell_x,
+            cellY: cell_y,
+            color: 'red',
+            text: "Cannot cast here, something's in the way",
+          });
         } else {
           window.pie.sendData({
             type: MESSAGE_TYPES.SPELL,
-            spell: { ...currentSpell, x: cell_x, y: cell_y },
+            spell: {
+              x: cell_x,
+              y: cell_y,
+              summon: { ...currentSpell.summon, vx: 0, vy },
+            },
           });
         }
+      } else {
+        window.pie.sendData({
+          type: MESSAGE_TYPES.SPELL,
+          spell: { ...currentSpell, x: cell_x, y: cell_y },
+        });
       }
-    } else {
-      console.error('Board is not the target of click', e.target);
     }
   });
   const elManaCost = document.getElementById('mana-cost');
