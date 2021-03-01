ee8c5ff7b20cb059b0fc2985824df170d53bad1c
Jordan O'Leary <jdoleary@gmail.com>
2021-03-01 15:56:53 -0500

    src: Add selecting spell within pool

--- src/SpellPool.ts
+++ src/SpellPool.ts
@@ -1,22 +1,36 @@
 import { MESSAGE_TYPES } from './MessageTypes';
-export interface ISpellPool {
-  spells: string[][];
-}
 const elPool = document.getElementById('spell-pool');
-export function create(): ISpellPool {
+let spells = [[], [], []];
+let selectedSpellIndex;
+export function selectSpell(index?: number) {
+  selectedSpellIndex = index;
+  // Deselect selected spell visually
+  document.querySelector('.spell.selected')?.classList.remove('selected');
+
+  const spell = spells[selectedSpellIndex];
+  if (spell) {
+    // Update the selected spell DOM element
+    document
+      .getElementById('spell-' + selectedSpellIndex)
+      .classList.add('selected');
+  }
+  // update tooltip with current state of clicked spell
+  window.setTooltip(JSON.stringify(spell || '', null, 2));
+}
+export function create(): string[][] {
   const elPoolSpells = document.querySelectorAll('#spell-pool .spell');
-  let self = {
-    spells: [[], [], []],
-  };
   for (let i = 0; i < elPoolSpells.length; i++) {
     const el: HTMLDivElement = document.querySelector(
       '#spell-pool #spell-' + i,
     );
+
+    // Click on spell
     // Add card to spell
     el.addEventListener('click', (e) => {
+      // Apply selected card to spell
       if (window.game.yourTurn && selectedCard) {
-        self.spells[i].push(selectedCard.content);
-        el.querySelector('.spell-content').innerHTML = self.spells[
+        spells[i].push(selectedCard.content);
+        el.querySelector('.spell-content').innerHTML = spells[
           i
         ].length.toString();
 
@@ -30,11 +44,11 @@ export function create(): ISpellPool {
         });
         selectedCard = null;
       }
-      // update tooltip with current state of clicked spell
-      window.setTooltip(JSON.stringify(self.spells[i], null, 2));
+      // Keep the last selected spell in state for casting later
+      selectSpell(i);
     });
   }
-  return self;
+  return spells;
 }
 let selectedCard: {
   content: string;
@@ -49,7 +63,11 @@ export function setSelectedCard(content: string, element: HTMLDivElement) {
     element,
   };
   if (selectedCard) {
+    // Add 'adding' class to the spell pool so spells change so as to suggest
+    // that they can accept the new spell
     elPool.classList.add('adding');
+    // Deselect spells once a card is selected so all spells appear the same
+    selectSpell(undefined);
   } else {
     elPool.classList.remove('adding');
   }
