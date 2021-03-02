ba1f60f1ca3fdfb0f997ae23e3b97fb312719689
Jordan O'Leary <jdoleary@gmail.com>
2021-03-02 09:37:06 -0500

    src: Lower unit base power to 1

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const UNIT_BASE_HEALTH = 4;
-export const UNIT_BASE_POWER = 2;
+export const UNIT_BASE_POWER = 1;
 export const PLAYER_HEART_HEALTH = 8;
 export const BOARD_HEIGHT = 8;
 export const BOARD_WIDTH = 8;
