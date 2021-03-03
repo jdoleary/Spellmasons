33167521adf25e27bac5402c81781681f6cb38cf
Jordan O'Leary <jdoleary@gmail.com>
2021-03-03 12:43:25 -0500

    config: 30 seconds per turn

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@ export const BOARD_HEIGHT = 8;
 export const BOARD_WIDTH = 8;
 export const CHOSEN_CARDS_TILL_NEXT_PHASE = 6;
 export const NUMBER_OF_UNITS_SPAWN_PER_TURN = 2;
-export const MAX_SECONDS_PER_TURN = 15;
+export const MAX_SECONDS_PER_TURN = 30;
