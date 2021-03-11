2c1055b7a16bd90a64810637c25fbaadf155c0e2
Jordan O'Leary <jdoleary@gmail.com>
2021-03-11 11:13:50 -0500

    clean: Remove old DOM image code

--- src/Image.ts
+++ src/Image.ts
@@ -55,29 +55,12 @@ export default class Image {
     // happen in board space occur in this "Image".  This is so that sub images can stay with
     // the "Image" at large while the unit itself can have independent rotation
     rotation = normalizeDegrees(rotation);
-    // this.unitImage = new SubImage(
-    //   { rotation },
-    //   CELL_SIZE,
-    //   CELL_SIZE,
-    //   imageName,
-    // );
     this.sprite = addPixiSprite(imageName);
     this.sprite.anchor.x = 0.5;
     this.sprite.anchor.y = 0.5;
     this.sprite.rotation = (rotation * Math.PI) / 180;
 
-    // this.sprite.appendChild(this.unitImage.element);
-    // this.elSubImageHolder = document.createElement('div');
-    // this.elSubImageHolder.classList.add('subimage-holder');
-    // this.sprite.appendChild(this.elSubImageHolder);
-    // this.sprite.style.width = `${CELL_SIZE}px`;
-    // this.sprite.style.height = `${CELL_SIZE}px`;
-    // this.sprite.id = `image-${Image.id}`;
-    // this.sprite.classList.add('game-image');
-    // Image.id++;
     this.set(cellX, cellY, 1.0);
-    // const boardContents = document.getElementById('board-contents');
-    // boardContents?.appendChild(this.sprite);
   }
   cellToBoardCoords(cell_x: number, cell_y: number) {
     return {
