6719e1d4b053f24412057482cddaca9cb2392d08
Jordan O'Leary <jdoleary@gmail.com>
2021-03-12 14:16:05 -0500

    fix: Fix floating text to work with PIXI js

--- src/FloatingText.ts
+++ src/FloatingText.ts
@@ -1,45 +1,44 @@
+import * as PIXI from 'pixi.js';
 import { CELL_SIZE } from './config';
+import { app } from './PixiUtils';
 interface FText {
   x: number;
   y: number;
   // velocity y
   vy: number;
-  opacity: number;
-  vopacity: number;
-  el: HTMLElement;
+  alpha: number;
+  valpha: number;
+  pixiText: PIXI.Text;
 }
 export default function floatingText({ cellX, cellY, text, color }) {
-  const el = document.createElement('div');
-  el.innerText = text;
-  el.classList.add('floating-text');
-  el.style.color = color;
-  document.getElementById('board')?.appendChild(el);
+  const pixiText = new PIXI.Text(text, { fill: color, align: 'center' });
   const instance = {
     // Place in the middle of cell
     x: cellX * CELL_SIZE + CELL_SIZE / 2,
     y: cellY * CELL_SIZE + CELL_SIZE / 2,
-    el,
+    pixiText,
     vy: 1,
-    opacity: 1,
-    vopacity: -0.2,
+    alpha: 1,
+    valpha: -0.2,
   };
+  app.stage.addChild(pixiText);
   requestAnimationFrame(() => floatAway(instance));
 }
 function floatAway(instance: FText) {
-  if (instance.el) {
+  if (instance.pixiText) {
     instance.y -= instance.vy;
     instance.vy = instance.vy * 0.97;
-    instance.opacity -= Math.max(instance.vopacity, 0);
-    instance.vopacity += 0.004;
-    instance.el.style.top = instance.y + 'px';
-    instance.el.style.left = instance.x + 'px';
-    instance.el.style.opacity = instance.opacity.toString();
+    instance.alpha -= Math.max(instance.valpha, 0);
+    instance.valpha += 0.004;
+    instance.pixiText.y = instance.y;
+    instance.pixiText.x = instance.x;
+    instance.pixiText.alpha = instance.alpha;
     // Once it's fully hidden / done animating
-    if (instance.opacity < 0) {
+    if (instance.alpha < 0) {
       // Clean up the element
-      instance.el.remove();
+      app.stage.removeChild(instance.pixiText);
       // Prevent continued looping
-      instance.el = null;
+      instance.pixiText = null;
     } else {
       requestAnimationFrame(() => floatAway(instance));
     }
