5620c1837492a4d5a5f32836f3fd0f1590e26d67
Jordan O'Leary <jdoleary@gmail.com>
2021-03-14 10:35:36 -0400

    fix: Player base health to use config

--- src/Player.ts
+++ src/Player.ts
@@ -4,17 +4,16 @@ import * as Unit from './Unit';
 export interface IPlayer {
   // wsPie id
   clientId: string;
-  heart_health: number;
   unit: Unit.IUnit;
   inPortal: boolean;
 }
 export function create(clientId: string): IPlayer {
   const player = {
     clientId,
-    heart_health: PLAYER_BASE_HEALTH,
     unit: Unit.create(0, 0, 'images/units/man-blue.png', 'PlayerControlled'),
     inPortal: false,
   };
+  player.unit.health = PLAYER_BASE_HEALTH;
   window.animationManager.startAnimate();
   return player;
 }
