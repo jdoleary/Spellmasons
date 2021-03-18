a3c98d4cda0b594aed912dc6cacfefbabd6916d5
Jordan O'Leary <jdoleary@gmail.com>
2021-03-18 11:46:56 -0400

    config: Reduce agro dist due to new distance alg
    Reduce unit health to 1 so fire spell is more effective
    Remove unused action_cost config vars

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const PLAYER_BASE_HEALTH = 8;
-export const UNIT_BASE_HEALTH = 4;
+export const PLAYER_BASE_HEALTH = 4;
+export const UNIT_BASE_HEALTH = 1;
 export const UNIT_BASE_POWER = 1;
 export const BOARD_HEIGHT = 9;
 export const BOARD_WIDTH = 12;
@@ -8,10 +8,6 @@ export const SECONDS_PER_TURN = 60;
 export const CELL_SIZE = 64;
 export const GIVE_NUM_CARDS_PER_LEVEL = 6;
 export const NUM_PICKUPS_PER_LEVEL = 2;
-// the number of actions a player can take in a turn
-export const PLAYER_ACTIONS_PER_TURN = 4;
-export const ACTION_COST_WALK = 3;
-export const ACTION_COST_CAST = 2;
 // How many times a single shield will block damage
 export const SHIELD_MULTIPLIER = 2;
-export const AI_AGRO_DISTANCE = 5;
+export const AI_AGRO_DISTANCE = 3;
