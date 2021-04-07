ac2e1103cd6265790808917a8e57f51a889ffdf0
Jordan O'Leary <jdoleary@gmail.com>
2021-04-07 12:48:41 -0400

    balance, give enemies more health

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const PLAYER_BASE_HEALTH = 4;
-export const UNIT_BASE_HEALTH = 1;
+export const UNIT_BASE_HEALTH = 4;
 export const UNIT_BASE_POWER = 1;
 export const BOARD_HEIGHT = 9;
 export const BOARD_WIDTH = 12;
@@ -8,7 +8,7 @@ export const CELL_SIZE = 64;
 export const GIVE_NUM_CARDS_PER_LEVEL = 9;
 export const NUM_PICKUPS_PER_LEVEL = 2;
 export const NUM_OBSTACLES_PER_LEVEL = 10;
-export const MILLIS_PER_ANIMATION = 50;
+export const MILLIS_PER_ANIMATION = 100;
 export const PERCENT_CHANCE_OF_HEAVY_UNIT = 10;
 export const NUMBER_OF_UPGRADES_TO_CHOOSE_FROM = 5;
 export const PLAYER_CAST_RANGE = 4;
