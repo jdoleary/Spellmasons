03be50df218f9da535ba48d1f44ed22d063feaa3
Jordan O'Leary <jdoleary@gmail.com>
2022-01-01 15:14:55 -0500

    html: Add readyState debug info to HTML

--- src/readyState.ts
+++ src/readyState.ts
@@ -8,10 +8,19 @@ const readyState = {
     underworld: false,
 }
 let is_fully_ready = false;
+const elReadyState = document.getElementById('ready-state');
 export function set(key: keyof typeof readyState, value: boolean) {
     readyState[key] = value;
     // If all values in readyState are true, then everything is ready
     is_fully_ready = Object.values(readyState).every(x => !!x)
+    if (elReadyState) {
+        elReadyState.innerHTML = JSON.stringify(readyState, null, 2);
+        if (is_fully_ready) {
+            // Don't show any debug information from readyState if it's
+            // fully ready
+            elReadyState.innerHTML = "";
+        }
+    }
     if (is_fully_ready) {
         // When the game is ready to process wsPie messages, begin
         // processing them
