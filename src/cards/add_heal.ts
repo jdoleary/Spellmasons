0bfdfec0fc62b6102d0184ca8a9d7789dea59176
Jordan O'Leary <jdoleary@gmail.com>
2022-01-05 12:59:40 -0500

    content: Make heal cost more mana

--- src/cards/add_heal.ts
+++ src/cards/add_heal.ts
@@ -3,6 +3,9 @@ import type { Spell } from '.';
 import { MANA_BASE_COST, MANA_MULTIPLIER_NONE } from '../config';
 
 const id = 'heal';
+const healAmount = 10;
+const manaCost = 20;
+
 const spell: Spell = {
   card: {
     id,
@@ -11,7 +14,7 @@ const spell: Spell = {
     description: `
 Heals all targets (no greater than maximum health).
     `,
-    manaCost: MANA_BASE_COST,
+    manaCost,
     manaMultiplier: MANA_MULTIPLIER_NONE,
     effect: async (state, dryRun) => {
       if (dryRun) {
@@ -20,7 +23,7 @@ Heals all targets (no greater than maximum health).
       for (let target of state.targets) {
         const unit = window.underworld.getUnitAt(target);
         if (unit) {
-          const damage = -1;
+          const damage = -healAmount;
           Unit.takeDamage(unit, damage);
           state.aggregator.damageDealt =
             (state.aggregator.damageDealt || 0) + damage;
