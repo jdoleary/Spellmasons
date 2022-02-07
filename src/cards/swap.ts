f3422d2f2693844964da2ae1af5148cb90cd0ae9
Jordan O'Leary <jdoleary@gmail.com>
2022-02-07 10:48:24 -0500

    fix: swap so it swaps with everyone

--- src/cards/swap.ts
+++ src/cards/swap.ts
@@ -18,33 +18,26 @@ Swaps the caster with the source target.
     manaMultiplier: MANA_MULTIPLIER_NONE,
     effect: async (state, dryRun) => {
       const { casterUnit, targets } = state;
-      const target = targets[0];
-      // Find movement change between caster and original target
-      const dx = targets[0].x - casterUnit.x;
-      const dy = targets[0].y - casterUnit.y;
       // Loop through all targets and batch swap locations
       const swapUnits: [Unit.IUnit, Vec2][] = [];
       const swapPickups: [Pickup.IPickup, Vec2][] = [];
-      const swapLocation = { x: target.x - dx, y: target.y - dy };
-      // The unit at the target location
-      const targetUnit = window.underworld.getUnitAt(target);
-      if (targetUnit) {
-        swapUnits.push([targetUnit, swapLocation]);
+      const swapLocation = { x: casterUnit.x, y: casterUnit.y };
+      // The unit at the location that the targetUnit will swap to
+      const swapUnit = window.underworld.getUnitAt(swapLocation);
+      if (swapUnit) {
+        swapUnits.push([swapUnit, targets[0]]);
       }
-      // The pickup at the target location
-      const pickupAtTarget = window.underworld.getPickupAt(target);
-      // Physically swap with pickups
-      if (pickupAtTarget) {
-        swapPickups.push([pickupAtTarget, swapLocation]);
-      }
-      // If there is no overlap between the swaplocation and the targets
-      if (
-        !targets.find((t) => t.x === swapLocation.x && t.y === swapLocation.y)
-      ) {
-        // The unit at the location that the targetUnit will swap to
-        const swapUnit = window.underworld.getUnitAt(swapLocation);
-        if (swapUnit) {
-          swapUnits.push([swapUnit, target]);
+      // The units at the target location
+      for (let target of targets) {
+        const targetUnit = window.underworld.getUnitAt(target);
+        if (targetUnit) {
+          swapUnits.push([targetUnit, swapLocation]);
+        }
+        // The pickup at the target location
+        const pickupAtTarget = window.underworld.getPickupAt(target);
+        // Physically swap with pickups
+        if (pickupAtTarget) {
+          swapPickups.push([pickupAtTarget, swapLocation]);
         }
         // The pickup at the swap location
         const pickupAtSwap = window.underworld.getPickupAt(swapLocation);
