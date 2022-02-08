ac36437745fab68ee6f6e781a934fc2cfee0fc69
Jordan O'Leary <jdoleary@gmail.com>
2022-02-08 08:49:37 -0500

    clean: Hide wall debug lines

--- src/ui/PlanningView.ts
+++ src/ui/PlanningView.ts
@@ -24,9 +24,9 @@ export function initPlanningView() {
 export function updatePlanningView() {
   planningViewGraphics.clear();
   // TODO: remove; Temp for testing, draw collision walls visually
-  for (let w of window.underworld.walls) {
-    drawSwapLine(w.p1, w.p2);
-  }
+  // for (let w of window.underworld.walls) {
+  //   drawSwapLine(w.p1, w.p2);
+  // }
   const mousePos = window.underworld.getMousePos();
   // Draw UI for units under the mouse on hover
   const unit = window.underworld.getUnitAt(mousePos);
