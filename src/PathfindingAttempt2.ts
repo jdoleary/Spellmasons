892db3937ff94b717af4c47fe631097aa219d179
Jordan O'Leary <jdoleary@gmail.com>
2022-03-01 13:12:20 -0500

    src: Poly: Add polygon functions
    getAngleBetweenAngles
    getAngleBetweenVec2s
    projectVertexAlongOutsideNormal
    
    all of these functions support insetPolygon which
    creates a new polygon with all of the verticies
    moved outward into empty space.  (For inverted
    polygons - like the game map bounds - the verticies move inward)

--- src/PathfindingAttempt2.ts
+++ src/PathfindingAttempt2.ts
@@ -1,5 +1,7 @@
 import type { LineSegment } from "./collision/collisionMath";
 import type { Vec2, Polygon, Vertex } from "./commonTypes";
+import * as vectorMath from './collision/vectorMath';
+import { distance, similarTriangles } from "./math";
 
 export function vec2sToPolygon(points: Vec2[]): Polygon {
     let startVertex;
@@ -22,12 +24,12 @@ export function vec2sToPolygon(points: Vec2[]): Polygon {
     return polygon;
 }
 
-export function polygonToVec2s(polygon: Polygon): Vec2[] {
+export function getVerticies(polygon: Polygon): Vertex[] {
     let currentVertex = polygon.startVertex;
-    let points = [];
+    let verticies: Vertex[] = [];
     let i = 0;
     do {
-        points.push({ x: currentVertex.x, y: currentVertex.y });
+        verticies.push(currentVertex);
         currentVertex = currentVertex.next;
         i++;
         // Arbitrary stop to prevent infinite loop
@@ -36,5 +38,85 @@ export function polygonToVec2s(polygon: Polygon): Vec2[] {
             break;
         }
     } while (polygon.startVertex != currentVertex);
-    return points;
+    return verticies;
+}
+export function polygonToVec2s(polygon: Polygon): Vec2[] {
+    return getVerticies(polygon).map(({ x, y }) => ({ x, y }));
+}
+// in radians
+function getAngleBetweenVec2s(v1: Vec2, v2: Vec2): number {
+    const dy = v2.y - v1.y;
+    const dx = v2.x - v1.x;
+    return Math.atan2(dy, dx);
+}
+// order matters
+// radians
+function getAngleBetweenAngles(anglePrev: number, angleNext: number): number {
+    const angleBetween = (anglePrev > angleNext ? anglePrev : anglePrev + Math.PI * 2) - angleNext;
+    return angleBetween
+}
+
+// Inset polygon: Grows a polygon into it's "outside" by the distance of magnitude
+// along the normal vectors of each vertex.
+// Uses the ordered verticies (from prev to next; running clockwise) to determine what is
+// "inside" and what is "outside"
+function insetPolygon(polygon: Polygon, magnitude: number): Polygon {
+    const vertices = getVerticies(polygon);
+    // Batch adjustedPoints and then adjust them all at once
+    const newPoints: Vec2[] = vertices.map(v => projectVertexAlongOutsideNormal(v, magnitude));
+    return vec2sToPolygon(newPoints)
+}
+// Returns a Vec2 of the vertex's coordinate projected along it's normal on the 
+// OUTSIDE side as specified by assuming that the outside is the angle from
+// vertex.prev to vertex.next
+function projectVertexAlongOutsideNormal(vertex: Vertex, magnitude: number): Vec2 {
+    // Find a point along the normal:
+    const projectToPoint = { x: vertex.x, y: vertex.y };
+    const dxPrev = vertex.x - vertex.prev.x;
+    const dyPrev = vertex.y - vertex.prev.y;
+    projectToPoint.x -= dxPrev;
+    projectToPoint.y -= dyPrev;
+    const dxNext = vertex.x - vertex.next.x;
+    const dyNext = vertex.y - vertex.next.y;
+    projectToPoint.x -= dxNext;
+    projectToPoint.y -= dyNext;
+
+    // Find out if the angle is inverted based on the order of prev and next verticiees
+    const anglePrev = getAngleBetweenVec2s(vertex, vertex.prev);
+    const angleNext = getAngleBetweenVec2s(vertex, vertex.next);
+    const angleBetween = getAngleBetweenAngles(anglePrev, angleNext);
+    const isInverted = angleBetween <= Math.PI / 2;
+    // console.log('angle', angleBetween);
+    const degToRad = Math.PI / 180;
+    // Find the point magnitude away from vertex along the normal
+    const X = projectToPoint.x - vertex.x;
+    const Y = projectToPoint.y - vertex.y;
+    const D = distance(projectToPoint, vertex);
+    const d = isInverted ? -magnitude : magnitude;
+    const relativeAdjustedPoint = similarTriangles(X, Y, D, d);
+    return vectorMath.subtract(vertex, relativeAdjustedPoint);
+}
+
+export const testables = {
+    insetPolygon,
+    projectVertexAlongOutsideNormal,
+    getAngleBetweenAngles,
+}
+
+// In order to pathfind, I need a non-intersecting convex polygon mesh.
+
+// The corner cases include walls that overlap, and insets that overlap.
+
+// How to solve:
+// 0. Start with collidable walls as Polygons (the Polygon interface is designed so it is clear what is inside the polygon and what is outside.  For example, the outer bounds of the game world is kind of an inverted polygon like the inside of a box is spacious and the entire outside is solid. Whereas obstacles are regular polygons where the inside is solid (you can't move through it) and the outside
+// is spacious and available for movement.  So inverted polygons can be expressed by the direction of prev and next in it's verticies.
+// --
+// Takes an array of Polygons and transforms them into a fully connected convex poly mesh
+export function generateConvexPolygonMesh(polys: Polygon[], insetSize: number): Polygon[] {
+    // 1. Grow the polygons according to `inset`.  Inset is used to give a margin to the pathing mesh so that units with thickness won't clip through walls as they pass by the corners or through a narrow area.
+    // 2. Solve for intersecting / overlapping polygons.  This step is important, for example if there is a very thin corridor and the inset is large enough, no space in the corridor will be pathable and this is because the collidable polygons will grow so much (due to the inset) that they will overlap.
+    // 3. Take the world bounds (the inverted polygon I mentioned before) and all the collidable polygons and make more connections between their verticies so that there are no concave polygons. This step will return a new array of polygons (probably 3-sided).
+    // 4.  Optimize the new array of polygons so that multiple polygons are combined if the unified polygon remains convex.
+    // 5.  Give polygons references to their neighbors (a neighboring polygon is any polygon that shares an edge
+    // 6. Use this array of polygons and their neighbors via an A* algorithm or something similar to pathfind.
 }
--- src/__test__/PathfindingAttempt2.test.ts
+++ src/__test__/PathfindingAttempt2.test.ts
@@ -1,5 +1,95 @@
-import { polygonToVec2s, vec2sToPolygon } from "../PathfindingAttempt2";
+import { polygonToVec2s, vec2sToPolygon, testables } from "../PathfindingAttempt2";
+const { projectVertexAlongOutsideNormal, insetPolygon, getAngleBetweenAngles } = testables;
 import type { Vec2 } from "../commonTypes";
+describe('projectVertexAlongOutsideNormal', () => {
+    it('should find the point "magnitude" distance away from p2 along the normal', () => {
+        // These points represent a corner, where the inside is down and to the right
+        // and the outside is up and to the left
+        // assuming a coordinate plane where right, up is x positive, y positive
+        const p1 = { x: 0, y: 0 }
+        const p2 = { x: 0, y: 1 }
+        const p3 = { x: 1, y: 1 }
+        const points: Vec2[] = [p1, p2, p3];
+        const polygon = vec2sToPolygon(points);
+
+        // get p2 as a vertex
+        const vertex = polygon.startVertex.next;
+        const actual = projectVertexAlongOutsideNormal(vertex, Math.sqrt(2));
+        const expected = { x: -1, y: 2 }
+        expect(actual).toEqual(expected);
+    });
+    it('should find the point "magnitude" distance away from p2 along the normal (inverted)', () => {
+        // These points represent a corner, where the inside is down and to the right
+        // and the outside is up and to the left
+        // assuming a coordinate plane where right, up is x positive, y positive
+        const p1 = { x: 0, y: 0 }
+        const p2 = { x: 0, y: 1 }
+        const p3 = { x: 1, y: 1 }
+        const points: Vec2[] = [p3, p2, p1];
+        const polygon = vec2sToPolygon(points);
+
+        // get p2 as a vertex
+        const vertex = polygon.startVertex.next;
+        const actual = projectVertexAlongOutsideNormal(vertex, Math.sqrt(2));
+        const expected = { x: 1, y: 0 }
+        expect(actual).toEqual(expected);
+    });
+});
+describe('getAngleBetweenAngles', () => {
+    [
+        [135, 45, 90],
+        [-90, 90, 180],
+        [0, 90, 270],
+        [90, 0, 90],
+        [180, 0, 180],
+        [-180, 0, 180],
+    ].map(([prev, next, expected]) => {
+        it(`should return ${expected} for ${prev} to ${next}`, () => {
+            const degToRad = Math.PI / 180;
+            const actual = getAngleBetweenAngles(prev * degToRad, next * degToRad);
+            expect(actual / degToRad).toEqual(expected);
+        });
+    });
+});
+
+describe('insetPolygon', () => {
+    it('should return a new polygon with all of its points grown by magnitude', () => {
+        const p1 = { x: 0, y: 0 }
+        const p2 = { x: 0, y: 1 }
+        const p3 = { x: 1, y: 1 }
+        const p4 = { x: 1, y: 0 }
+        const points: Vec2[] = [p1, p2, p3, p4];
+        const polygon = vec2sToPolygon(points);
+
+        const newPolygon = insetPolygon(polygon, Math.sqrt(2));
+        const newPoints = polygonToVec2s(newPolygon);
+        expect(newPoints).toEqual([
+            { x: -1, y: -1 },
+            { x: -1, y: 2 },
+            { x: 2, y: 2 },
+            { x: 2, y: -1 },
+        ]);
+    });
+    it('should inset in the opposite direction for inverted polygons where the inside and outside are flipped', () => {
+        const p1 = { x: 0, y: 0 }
+        const p2 = { x: 0, y: 2 }
+        const p3 = { x: 2, y: 2 }
+        const p4 = { x: 2, y: 0 }
+        // Note: The points are reversed which means the polygon is inverted
+        const points: Vec2[] = [p1, p2, p3, p4].reverse();
+        const polygon = vec2sToPolygon(points);
+
+        const newPolygon = insetPolygon(polygon, Math.sqrt(2));
+        const newPoints = polygonToVec2s(newPolygon);
+        expect(newPoints).toEqual([
+            { x: 1, y: 1 },
+            { x: 1, y: 1 },
+            { x: 1, y: 1 },
+            { x: 1, y: 1 },
+        ]);
+    });
+
+});
 describe('vec2sToPolygon', () => {
     it('should create a polygon from the Vec2s', () => {
         const p1 = { x: -1, y: 0 }
@@ -24,7 +114,6 @@ describe('vec2sToPolygon', () => {
 
         expect(polygon.startVertex.prev.next.x).toEqual(p1.x);
         expect(polygon.startVertex.prev.next.y).toEqual(p1.y);
-
     });
 });
 describe('polygonToVec2s', () => {
