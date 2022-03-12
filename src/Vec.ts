8b296943d96d7b00996fdadfa3db8fd3d8f5c5f4
Jordan O'Leary <jdoleary@gmail.com>
2022-03-12 11:00:06 -0500

    clean: Clarify params

--- src/Vec.ts
+++ src/Vec.ts
@@ -4,9 +4,9 @@ export interface Vec2 {
     y: number;
 }
 
-// in radians
-export function getAngleBetweenVec2s(v1: Vec2, v2: Vec2): number {
-    const dy = v2.y - v1.y;
-    const dx = v2.x - v1.x;
+// Get the angle away from the x-axis from origin to point in radians
+export function getAngleBetweenVec2s(origin: Vec2, point: Vec2): number {
+    const dy = point.y - origin.y;
+    const dx = point.x - origin.x;
     return Math.atan2(dy, dx);
 }
