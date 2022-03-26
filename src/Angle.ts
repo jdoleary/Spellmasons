297ba8a113fd4b8f427fc580612a60e4948f5698
Jordan O'Leary <jdoleary@gmail.com>
2022-03-26 16:58:59 -0400

    fix: Angle.ts
    This project only uses clockwiseAngles.
    Fixes isAngleBetweenAngles to see if angle1
    is between clockwise of angle 2 and counterclockwise of angle3

--- src/Angle.ts
+++ src/Angle.ts
@@ -1,22 +1,21 @@
 
 // Find the angle between two angles going counter-clockwise around the angle circle
-// So 135deg to -135deg is 45deg because -135deg == 225deg
-export function counterClockwiseAngle(rad1: number, rad2: number): number {
-    const shouldInvert = rad1 > rad2;
-    // Convert rad2 into an identical angle that is larger than rad1
-    const adjustedRad2 = shouldInvert ? rad2 + Math.PI * 2 : rad2;
-    const result = adjustedRad2 - rad1;
+// So -135deg to 135deg is 45deg because -135deg == 225deg
+export function clockwiseAngle(rad1: number, rad2: number): number {
+    rad1 = normalizeAngle(rad1);
+    rad2 = normalizeAngle(rad2);
+    const shouldInvert = rad2 > rad1;
+    // Convert rad1 into an identical angle that is larger than rad2
+    const adjustedRad1 = shouldInvert ? rad1 + Math.PI * 2 : rad1;
+    const result = adjustedRad1 - rad2;
     // Normalize between 0 and Math.PI*2
     const normalizedResult = result > Math.PI * 2 ? result % (Math.PI * 2) : result;
     return normalizedResult;
 }
-export function clockwiseAngle(rad1: number, rad2: number): number {
-    return counterClockwiseAngle(rad2, rad1);
-}
-// returns the angle between (-Math.PI, Math.PI]
+// returns the angle between [0, Math.PI * 2)
 export function normalizeAngle(radians: number): number {
     const n1 = radians % (Math.PI * 2);
-    if (n1 <= -Math.PI) {
+    if (n1 < 0) {
         return n1 + Math.PI * 2;
     } else {
         return n1;
@@ -24,9 +23,10 @@ export function normalizeAngle(radians: number): number {
     }
 }
 
-// Returns true if angle1 is between angle 2 and 3
+// Returns true if angle1 is between angle 2 and 3 when going clockwise from
+// angle2 to angle3
 export function isAngleBetweenAngles(angle1: number, angle2: number, angle3: number): boolean {
-    const from2To1 = counterClockwiseAngle(angle2, angle1);
-    const from2To3 = counterClockwiseAngle(angle2, angle3);
+    const from2To1 = clockwiseAngle(angle2, angle1);
+    const from2To3 = clockwiseAngle(angle2, angle3);
     return from2To1 < from2To3;
 }
--- src/__test__/Angle.test.ts
+++ src/__test__/Angle.test.ts
@@ -1,26 +1,28 @@
-import { counterClockwiseAngle, normalizeAngle, isAngleBetweenAngles } from '../Angle';
+import { clockwiseAngle, normalizeAngle, isAngleBetweenAngles } from '../Angle';
 describe('Angle', () => {
-    describe('counterClockwiseAngle', () => {
+    describe('clockwiseAngle', () => {
         const testPairs = [
-            [135, -135, 90],
-            [-45, 45, 90],
-            [45, -45, 270],
-            [-90, 355, 85]
+            [-135, 135, 90],
+            [45, -45, 90],
+            [-45, 45, 270],
+            [355, -90, 85],
+            [-90, 0, 270],
+            // floating point error, but it works good enough:
+            [-90, 359, 271.00000000000006],
         ];
         for (let [from, to, expected] of testPairs) {
-            it(`should return the angle going counterclockwise between the angles ${from} and ${to}`, () => {
-                const actual = counterClockwiseAngle(from * Math.PI / 180, to * Math.PI / 180) * 180 / Math.PI;
+            it(`should return the angle ${expected} going counterclockwise between the angles ${from} and ${to}`, () => {
+                const actual = clockwiseAngle(from * Math.PI / 180, to * Math.PI / 180) * 180 / Math.PI;
                 expect(actual).toEqual(expected);
             });
         }
     });
     describe("normalizeAngle", () => {
-        it("should return an angle between -Math.PI and Math.PI (positive example)", () => {
+        it("should return an angle between 0 and Math.PI*2 (positive example)", () => {
             const angle = Math.PI * 5;
             const actual = normalizeAngle(angle);
             const expected = Math.PI;
             expect(actual).toEqual(expected);
-
         });
         it("should not change the angle if it is already between 0 and 360", () => {
             const angle = Math.PI / 2;
@@ -29,21 +31,28 @@ describe('Angle', () => {
             expect(actual).toEqual(expected);
 
         });
-        it("should return an angle between -Math.PI and Math.PI (negative example)", () => {
+        it("should return an angle between 0 and Math.PI *2 (negative example)", () => {
             const angle = -3 * Math.PI / 2;
             const actual = normalizeAngle(angle) * 180 / Math.PI;
             const expected = 90;
+            expect(actual).toEqual(expected);
+        });
+        it("should return an angle between 0 and Math.PI*2", () => {
+            const angle = -Math.PI / 2;
+            const actual = normalizeAngle(angle);
+            const expected = 3 * Math.PI / 2;
             expect(actual).toEqual(expected);
         });
 
     });
     describe("isAngleBetweenAngles", () => {
         const testGroups: [number, number, number, boolean][] = [
-            [0, -Math.PI / 2, Math.PI / 2, true],
-            [2 * Math.PI - 0.1, -Math.PI / 2, Math.PI / 2, true],
-            [0, Math.PI / 2, -Math.PI / 2, false],
-            [Math.PI, Math.PI / 2, -Math.PI / 2, true],
-            [-3 * Math.PI / 4, Math.PI / 2, 0, true],
+            [0, -Math.PI / 2, Math.PI / 2, false],
+            [2 * Math.PI - 0.1, -Math.PI / 2, Math.PI / 2, false],
+            // [0, Math.PI / 2, -Math.PI / 2, true],
+            // [Math.PI, Math.PI / 2, -Math.PI / 2, false],
+            // [-3 * Math.PI / 4, Math.PI / 2, 0, false],
+            // [-91 * Math.PI / 180, -90 * Math.PI / 180, 0, true]
 
         ];
         for (let [testAngle, angleLowerBound, angleUpperBound, expected] of testGroups) {
