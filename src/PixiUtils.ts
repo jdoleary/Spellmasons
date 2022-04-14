f6d33412210a5f90a70dfc8c2de3d13fa10981c3
Jordan O'Leary <jdoleary@gmail.com>
2022-04-14 15:06:14 -0400

    doc: Comments for camera movement

--- src/PixiUtils.ts
+++ src/PixiUtils.ts
@@ -85,9 +85,11 @@ export function recenterCamera() {
         const mapRightMostPoint = window.underworld.width + margin;
         const camCenterXMin = mapLeftMostPoint + window.innerWidth / 2 / zoom;
         const camCenterXMax = mapRightMostPoint - window.innerWidth / 2 / zoom;
+        // If the supposed minimum is more than the maximum, just center the camera:
         if (camCenterXMin > camCenterXMax) {
           centerTarget.x = (mapRightMostPoint - mapLeftMostPoint) / 2;
         } else {
+          // clamp the camera x between the min and max possible camera targets
           centerTarget.x = Math.min(camCenterXMax, Math.max(camCenterXMin, centerTarget.x));
         }
         //Clamp camera Y
@@ -95,14 +97,28 @@ export function recenterCamera() {
         const mapBottomMostPoint = window.underworld.height + margin;
         const camCenterYMin = mapTopMostPoint + window.innerHeight / 2 / zoom;
         const camCenterYMax = mapBottomMostPoint - window.innerHeight / 2 / zoom;
+        // If the supposed minimum is more than the maximum, just center the camera:
         if (camCenterYMin > camCenterYMax) {
           centerTarget.y = (mapBottomMostPoint - mapTopMostPoint) / 2;
         } else {
+          // clamp the camera x between the min and max possible camera targets
           centerTarget.y = Math.min(camCenterYMax, Math.max(camCenterYMin, centerTarget.y));
         }
-        // Center the camera on centerTarget
-        app.stage.x = window.innerWidth / 2 - (centerTarget.x * zoom);
-        app.stage.y = window.innerHeight / 2 - (centerTarget.y * zoom);
+
+        // Actuall move the camera to be centered on the centerTarget
+        const cameraTarget = {
+          x: window.innerWidth / 2 - (centerTarget.x * zoom),
+          y: window.innerHeight / 2 - (centerTarget.y * zoom)
+        }
+
+        // Option 1 for cam movement: Lerp camera to target
+        // app.stage.x = app.stage.x + (cameraTarget.x - app.stage.x) / 2;
+        // app.stage.y = app.stage.y + (cameraTarget.y - app.stage.y) / 2;
+
+        // Option 2 for cam movement: Set camera to target immediately
+        app.stage.x = cameraTarget.x;
+        app.stage.y = cameraTarget.y;
+
       }
       break;
   }
