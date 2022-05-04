473f44589a3491b3245259ef1fe13696e714c124
Jordan O'Leary <jdoleary@gmail.com>
2022-05-04 19:38:57 -0400

    fix: Restore random seed from dev work

--- src/Underworld.ts
+++ src/Underworld.ts
@@ -101,8 +101,7 @@ export default class Underworld {
 
   constructor(seed: string, RNGState: SeedrandomState | boolean = true) {
     window.underworld = this;
-    // this.seed = seed;
-    this.seed = '0.7515716568409228';
+    this.seed = seed;
     console.log("RNG create with seed:", seed, ", state: ", RNGState);
     this.random = this.syncronizeRNG(RNGState);
 
