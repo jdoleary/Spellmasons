cac56e95526d9b7b75b8978990bb3ac893bb59ee
Jordan O'Leary <jdoleary@gmail.com>
2022-05-09 17:51:48 -0400

    src: Make cookie consent more obvious

--- src/cookieConsent.ts
+++ src/cookieConsent.ts
@@ -8,18 +8,23 @@ export default function cookieConsentPopup(forcePopup: boolean) {
     const el = document.createElement('div')
     document.body.appendChild(el);
     el.innerHTML = `
-<div id="cookie-consent" style="border:1px solid black; padding:4px; position:fixed;left:0;right:0;bottom:0;background-color:white;z-index:1;">
-	<p>
-	This website uses cookies, localStorage, and sessionStorage to save relevant information for playing Spellmasons such as your clientID (used to distinguish you from other online players), and the game state (which represents the information needed to play the game).
-	</p>
-	<div>
-        <button id="cookie-consent-allow" type="button">
-            Allow
-        </button>
-        <button id="cookie-consent-deny" type="button">
-            Deny
-        </button>
-	</div>
+<div id="cookie-consent">
+    <div id="cookie-consent-inner">
+        <p>
+        Spellmasons uses cookies, localStorage, and sessionStorage to save relevant information for playing Spellmasons such as your clientID (used to distinguish you from other online players), and the game state (which represents the information needed to play the game).
+        </p>
+        <p>
+        Please allow cookies for the best user experience
+        </P>
+        <div>
+            <button id="cookie-consent-allow" type="button">
+                Allow
+            </button>
+            <button id="cookie-consent-deny" type="button">
+                Deny
+            </button>
+        </div>
+    </div>
 </div>
 `;
     const elAllow = document.getElementById('cookie-consent-allow');
@@ -36,5 +41,6 @@ function deny() {
     window.allowCookies = false;
     localStorage.clear();
     document.getElementById('cookie-consent')?.remove();
+
 }
 window.cookieConsentPopup = cookieConsentPopup;
