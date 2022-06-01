2fdc69dc2c1e94d5340376d7ff7c394d2302c063
Jordan O'Leary <jdoleary@gmail.com>
2022-06-01 10:14:07 -0400

    src: Update Conway to use
    new Material type.
    fix:  if(tile) returning falsey with undefined
    check for Material.EMPTY since Material.EMPTY is 0

--- src/Conway.ts
+++ src/Conway.ts
@@ -1,42 +1,48 @@
-import { CaveTile, Materials } from "./MapOrganicCave";
 import { randInt } from "./rand";
 import type { Vec2 } from "./Vec"
 import { oneDimentionIndexToVec2 } from "./WaveFunctionCollapse";
+export enum Material {
+    EMPTY,
+    LIQUID,
+    GROUND,
+    WALL,
+    SEMIWALL
+}
 
 // Neighbors
 // 0,1,2
 // 7   3
 // 6,5,4
-function mutateViaRules(tile: CaveTile, neighbors: (CaveTile | undefined)[], state: ConwayState): CaveTile {
+function mutateViaRules(tile: Material, neighbors: (Material | undefined)[], state: ConwayState): Material {
     // Replace empty tiles
-    if (tile.material == Materials.Empty) {
-        if (neighbors.some(t => t && t.material == Materials.Ground)) {
-            // If any neighbor is Ground, make it a wall
-            return { ...tile, material: Materials.Wall }
-        } else if (neighbors.some(t => t && t.material == Materials.Wall)) {
-            // If no neighbors are Ground and any neighbor is wall, make it a semi-wall
-            return { ...tile, material: Materials.SemiWall }
+    if (tile == Material.EMPTY) {
+        if (neighbors.some(t => t && t == Material.GROUND)) {
+            // If any neighbor is GROUND, make it a wall
+            return Material.WALL
+        } else if (neighbors.some(t => t && t == Material.WALL)) {
+            // If no neighbors are GROUND and any neighbor is wall, make it a semi-wall
+            return Material.SEMIWALL
         }
     }
     // Given a ground tile
-    if (tile.material == Materials.Ground) {
+    if (tile == Material.GROUND) {
         if (state.currentNumberOfLiquidPools < state.desiredNumberOfLiquidPools) {
             // If all of it's neighbors are ground it is a candidate for liquid pool
-            if (neighbors.every(t => t && t.material == Materials.Ground)) {
+            if (neighbors.every(t => t && t == Material.GROUND)) {
                 state.currentNumberOfLiquidPools++;
-                return { ...tile, material: Materials.Liquid }
+                return Material.LIQUID
             }
         }
 
         // Grow liquid pools
         // If at least one neighbor is liquid
-        if (neighbors.some(t => t && t.material == Materials.Liquid)) {
+        if (neighbors.some(t => t == Material.LIQUID)) {
             // and all other neighbors are ground (so liquid doesn't butt up against walls and block pathing)
-            if (neighbors.every(t => t && t.material == Materials.Ground || t?.material == Materials.Liquid)) {
+            if (neighbors.every(t => t && t == Material.GROUND || t == Material.LIQUID)) {
                 const roll = randInt(window.underworld.random, 0, 1)
                 // chance of changing it to liquid and growing the pool
                 if (roll == 0) {
-                    return { ...tile, material: Materials.Liquid }
+                    return Material.LIQUID
                 }
 
             }
@@ -55,7 +61,7 @@ function vec2ToOneDimentionIndexPreventWrap(pos: Vec2, width: number): number {
     return pos.y * width + pos.x
 
 }
-export function getNeighbors(tileIndex: number, tiles: CaveTile[], widthOf2DArray: number): (CaveTile | undefined)[] {
+export function getNeighbors(tileIndex: number, tiles: Material[], widthOf2DArray: number): (Material | undefined)[] {
     const { x, y } = oneDimentionIndexToVec2(tileIndex, widthOf2DArray);
     const neighbors = [
         tiles[vec2ToOneDimentionIndexPreventWrap({ x: x - 1, y: y - 1 }, widthOf2DArray)],
@@ -76,10 +82,10 @@ export interface ConwayState {
     currentNumberOfLiquidPools: number;
     desiredNumberOfLiquidPools: number;
 }
-export function conway(tiles: CaveTile[], widthOf2DArray: number, state: ConwayState) {
+export function conway(tiles: Material[], widthOf2DArray: number, state: ConwayState) {
     for (let i = 0; i < tiles.length; i++) {
         const tile = tiles[i];
-        if (tile) {
+        if (tile !== undefined) {
             const neighbors = getNeighbors(i, tiles, widthOf2DArray);
             tiles[i] = mutateViaRules(tile, neighbors, state);
         }
