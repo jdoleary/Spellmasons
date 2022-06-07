fdcbe05ccba8cbf25d0d8d03880c9b568435c0a0
Jordan O'Leary <jdoleary@gmail.com>
2022-06-07 15:30:01 -0400

    shader: Add color replace shader to player

--- src/ui/colors.ts
+++ src/ui/colors.ts
@@ -3,3 +3,6 @@ export const stamina = 0xd5b356;
 export const healthHurtRed = 0x632828;
 export const healthRed = 0xd55656;
 export const healthAllyGreen = 0x40a058;
+
+export const playerCoatPrimary = 0x417F81;
+export const playerCoatSecondary = 0x70A5A7;
