5c52a2c44753b74797abcdb35a4bb29623baddfc
Jordan O'Leary <jdoleary@gmail.com>
2022-07-03 09:12:25 -0400

    chore: Increase build.chunkSizeWarningLimit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default defineConfig(({ command, mode }) => ({
+    build: {
+        chunkSizeWarningLimit: 1000000
+    }
+}));
