5e38bd7c4c2a3364175a9a8e41491634deeaff2a
Jordan O'Leary <jdoleary@gmail.com>
2022-07-11 08:13:43 -0400

    src: Change raceTimeout log to error so that it is captured by Sentry

--- src/Promise.ts
+++ src/Promise.ts
@@ -1,7 +1,7 @@
 export function raceTimeout(ms: number, message: string, promise: Promise<any>): Promise<any> {
     return new Promise((resolve, reject) => {
         let timeoutId = setTimeout(() => {
-            console.log('raceTimeout: ', message);
+            console.error('raceTimeout: ', message);
             resolve(undefined);
         }, ms)
         promise.then(x => {
