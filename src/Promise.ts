9368369f30f5e222b98d97922de17261538b429d
Jordan O'Leary <jdoleary@gmail.com>
2022-07-11 09:10:08 -0400

    log: Add debug message measuring the execution times of promises so that I can tune the timeouts. The timeouts are intented to prevent the game hanging on await. This function will ensure the game doesn't hang and will log an error (which will be picked up by sentry) so I know to investigate

--- src/Promise.ts
+++ src/Promise.ts
@@ -1,11 +1,19 @@
 export function raceTimeout(ms: number, message: string, promise: Promise<any>): Promise<any> {
+    const start = Date.now();
     return new Promise((resolve, reject) => {
         let timeoutId = setTimeout(() => {
             console.error('raceTimeout: ', message);
             resolve(undefined);
         }, ms)
         promise.then(x => {
+            // Debug message to see how long it takes the promise to resolve naturally
+            if (!message.includes('moveTowards')) {
+                console.debug(message, 'resolved in', Date.now() - start)
+            }
+            // Ensure that the timeout doesn't trigger now that the promise has resolved
+            // natually within the allowed time
             clearTimeout(timeoutId);
+            // Pass on the result of the promise
             resolve(x);
         }).catch(reject);
     });
