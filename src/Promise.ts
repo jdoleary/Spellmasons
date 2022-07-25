23ca05779edd45fa0a95744968ec017419190516
Jordan O'Leary <jdoleary@gmail.com>
2022-07-25 06:48:13 -0400

    log: Clean up noisy debug log for raceTimeout

--- src/Promise.ts
+++ src/Promise.ts
@@ -6,11 +6,6 @@ export function raceTimeout(ms: number, message: string, promise: Promise<any>):
             resolve(undefined);
         }, ms)
         promise.then(x => {
-            // Debug message to see how long it takes the promise to resolve naturally
-            // TODO: Remove debug message when no longer needed (for prod)
-            if (!message.includes('moveTowards')) {
-                console.debug(message, 'resolved in', Date.now() - start)
-            }
             // Ensure that the timeout doesn't trigger now that the promise has resolved
             // natually within the allowed time
             clearTimeout(timeoutId);
