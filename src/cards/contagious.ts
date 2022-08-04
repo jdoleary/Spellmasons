be7d933bc911e69c6767bab6fbedd4300ab9508e
Jordan O'Leary <jdoleary@gmail.com>
2022-08-04 11:50:21 -0400

    content: refactor contagious so that it spreads
    immediately and not as a curse

--- src/cards/contagious.ts
+++ src/cards/contagious.ts
@@ -8,22 +8,9 @@ import floatingText from '../graphics/FloatingText';
 import * as Unit from '../entity/Unit';
 import Underworld from '../Underworld';
 import { CardCategory } from '../types/commonTypes';
+import { drawPredictionCircle } from '../graphics/PlanningView';
 
 const id = 'contagious';
-const imageName = 'contagious.png'
-function add(unit: IUnit) {
-  // Note: Curse can stack multiple times but doesn't keep any state
-  // so it doesn't need a first time setup like freeze does
-
-  unit.modifiers[id] = { isCurse: true };
-  // Add event
-  // Note: contagious should always be the first modifier or else other modifiers may expire before they can spread
-  // for example, freeze then contagious.  This is why it unshifts the contagious eventn
-  // TODO: This may require a pre-turn-start event phase
-  unit.onTurnStartEvents.unshift(id);
-  // Add subsprite image
-  Image.addSubSprite(unit.image, id);
-}
 
 const spell: Spell = {
   card: {
@@ -35,71 +22,58 @@ const spell: Spell = {
     probability: 5,
     thumbnail: 'contagious.png',
     description: `
-Makes this unit's curses contagious to other nearby units
+Immediately spreads this unit's curses to other nearby units.
     `,
     effect: async (state, card, quantity, underworld, prediction) => {
       for (let unit of state.targetedUnits) {
-        // Don't add contagious more than once
-        if (!unit.onTurnStartEvents.includes(id)) {
-          Unit.addModifier(unit, id, underworld, prediction);
-        }
+        await spreadCurses(unit, underworld, prediction);
       }
       return state;
     },
   },
-  modifiers: {
-    add,
-    subsprite: {
-      imageName,
-      alpha: 1.0,
-      anchor: {
-        x: 0,
-        y: 0,
-      },
-      scale: {
-        x: 0.5,
-        y: 0.5,
-      },
-    },
-
-  },
-  events: {
-    onTurnStart: async (unit: IUnit, prediction: boolean, underworld: Underworld) => {
-      const nearByUnits = underworld.getUnitsWithinDistanceOfTarget(unit, COLLISION_MESH_RADIUS * 4, prediction)
-        // Filter out undefineds
-        .filter(x => x !== undefined)
-        // Do not spread to dead units
-        .filter(x => x?.alive)
-        // Filter out self
-        .filter(x => x !== unit) as IUnit[];
-      const curseCards: ICard[] = Object.entries(unit.modifiers)
-        // Only curses are contagious
-        // Do not make contagious itself contagious
-        .filter(([cardId, modValue]) => modValue.isCurse && cardId !== id)
-        .map(([id, _mod]) => allCards[id])
-        .filter(x => x !== undefined) as ICard[];
-      for (let card of curseCards) {
-        const promises = [];
-        // Filter out units that already have this curse
-        for (let touchingUnit of nearByUnits.filter(u => !Object.keys(u.modifiers).includes(card.id))) {
-          if (!prediction) {
-            // Visually show the contageon
-            promises.push(createVisualLobbingProjectile(
-              unit,
-              touchingUnit,
-              'green-thing.png',
-            ).then(() => {
-              floatingText({ coords: touchingUnit, text: card.id });
-            }));
-          }
-          Unit.addModifier(touchingUnit, card.id, underworld, prediction);
-        }
-        await Promise.all(promises);
-
-      }
-
-      return false;
-    },
-  },
 };
 export default spell;
+
+async function spreadCurses(unit: IUnit, underworld: Underworld, prediction: boolean) {
+  const range = COLLISION_MESH_RADIUS * 4;
+  drawPredictionCircle(unit, range, 'Contagion Radius');
+  const nearByUnits = underworld.getUnitsWithinDistanceOfTarget(unit, range, prediction)
+    // Filter out undefineds
+    .filter(x => x !== undefined)
+    // Do not spread to dead units
+    .filter(x => x?.alive)
+    // Filter out self
+    .filter(x => x !== unit) as IUnit[];
+  const curseCards: ICard[] = Object.entries(unit.modifiers)
+    // Only curses are contagious
+    // Do not make contagious itself contagious
+    .filter(([cardId, modValue]) => modValue.isCurse && cardId !== id)
+    .map(([id, _mod]) => allCards[id])
+    .filter(x => x !== undefined) as ICard[];
+
+  for (let card of curseCards) {
+    const promises = [];
+    // Filter out units that already have this curse
+    for (let touchingUnit of nearByUnits.filter(u => !Object.keys(u.modifiers).includes(card.id))) {
+      let animationPromise = Promise.resolve();
+      if (!prediction) {
+        // Visually show the contageon
+        animationPromise = createVisualLobbingProjectile(
+          unit,
+          touchingUnit,
+          'projectile/poisonerProjectile',
+        ).then(() => {
+          floatingText({ coords: touchingUnit, text: card.id });
+        });
+        promises.push(animationPromise);
+      }
+      // Spread the curse after the animation promise completes
+      animationPromise.then(() => {
+        Unit.addModifier(touchingUnit, card.id, underworld, prediction);
+      });
+    }
+    await Promise.all(promises);
+
+  }
+
+}
