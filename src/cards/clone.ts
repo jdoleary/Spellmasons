6c05a22deafba8b4e11d5f859fe6d5562e63a338
Jordan O'Leary <jdoleary@gmail.com>
2022-08-11 11:16:30 -0400

    art: Make clone mitosis animations happen all at once for multiple targets

--- src/cards/clone.ts
+++ src/cards/clone.ts
@@ -31,6 +31,26 @@ Clones each target
       for (let target of targets) {
         clonePairs.push([target, { x: target.x, y: target.y }]);
       }
+      let animationPromise = Promise.resolve();
+      // Animate all the clonings
+      for (let [target, cloneSourceCoords] of clonePairs) {
+        if (target) {
+          const unit = underworld.getUnitAt(target, prediction);
+          // Since pickups aren't currently considered in prediction predictions just return undefined
+          // if this is a prediction or else it will ACTUALLY clone pickups when just making predictions
+          // 2022-05-09
+          const pickup = prediction ? undefined : underworld.getPickupAt(target, prediction);
+          if (unit) {
+            animationPromise = animateMitosis(unit.image);
+          }
+          if (pickup) {
+            animationPromise = animateMitosis(pickup.image);
+          }
+        }
+      }
+      // Note: animationPromise is overwritten over and over because each animateMitosis will take the same amount of time
+      // and they are all triggered at once so we only need to wait for one of them.
+      await animationPromise;
       // Clone all the batched clone jobs
       for (let [target, cloneSourceCoords] of clonePairs) {
         if (target) {
@@ -43,7 +63,6 @@ Clones each target
           // If there is are clone coordinates to clone into
           if (cloneSourceCoords) {
             if (unit) {
-              await animateMitosis(unit.image);
               // Jitter prevents multiple clones from spawning on top of each other
               const validSpawnCoords = underworld.findValidSpawn(jitter(cloneSourceCoords, config.COLLISION_MESH_RADIUS / 2, underworld.random), 5, 10);
               if (validSpawnCoords) {
@@ -65,7 +84,6 @@ Clones each target
             }
             if (pickup) {
               const validSpawnCoords = underworld.findValidSpawn(cloneSourceCoords, 5, 20)
-              await animateMitosis(pickup.image);
               if (validSpawnCoords) {
                 const clone = Pickup.load(pickup, underworld, prediction);
                 Pickup.setPosition(clone, validSpawnCoords.x, validSpawnCoords.y);
