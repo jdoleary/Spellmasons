6b479594364b553efeddfae4dd7480df547147b7
Jordan O'Leary <jdoleary@gmail.com>
2022-08-15 15:30:21 -0400

    art: Add effect to mana burn

--- src/cards/mana_burn.ts
+++ src/cards/mana_burn.ts
@@ -1,5 +1,6 @@
 import * as Unit from '../entity/Unit';
 import { CardCategory } from '../types/commonTypes';
+import { playDefaultSpellAnimation } from './cardUtils';
 import { Spell } from './index';
 
 const id = 'mana_burn';
@@ -23,6 +24,7 @@ Burn up to ${mana_burnt} of the targets' mana, causing the target take ${health_
         const unitManaBurnt = Math.min(unit.mana, mana_burnt);
         unit.mana -= unitManaBurnt;
         const damage = unitManaBurnt * health_burn_ratio
+        await playDefaultSpellAnimation(card, state.targetedUnits, prediction);
         Unit.takeDamage(unit, damage, unit, underworld, prediction, state);
       }
       return state;
