9e6c4467232670c673603184098ed31f6fa871c8
Jordan O'Leary <jdoleary@gmail.com>
2022-08-26 10:34:06 -0400

    ref: Remove click outside to cancel jprompt
    Jprompt should be handled with one of it's buttons.
    This prevents Jprompt from being dismissed if it comes up while
    user is moving with RMB

--- src/graphics/Jprompt.ts
+++ src/graphics/Jprompt.ts
@@ -45,25 +45,6 @@ export default async function Jprompt(prompt: Prompt): Promise<boolean> {
                 return false;
             });
         }
-        // Click outside is the same as no
-        // unless there is only a yes button, then it clicks yes
-        const cancelFn = (e: MouseEvent) => {
-            // Only cancel for left click
-            // Ignore clicks on the prompt inner
-            if (e.button == 0 && !(e.target && (e.target as Element).closest('.prompt-inner'))) {
-                // Handle clicks outside the prompt the same as if the user clicked the no/cancel button
-                if (noBtn) {
-                    noBtn.click();
-                } else if (yesBtn) {
-                    // If there is only a yesBtn, then a click outside would represent "Acknoledged"
-                    yesBtn.click();
-                }
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        }
-        el.addEventListener('click', cancelFn);
-        el.addEventListener('contextmenu', cancelFn);
         if (yesBtn) {
             yesBtn.addEventListener('click', (e) => {
                 res(true);
