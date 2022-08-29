56bb5814037915be9c23b2404bd4a9c1e9c21943
Jordan O'Leary <jdoleary@gmail.com>
2022-08-29 10:58:17 -0400

    art: Add vampire shadow

--- images/units/composite.js
+++ images/units/composite.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const { exec } = require("child_process");
 
-const onlyList = ['priest']
+const onlyList = ['vampire']
 const greenList = ['Attack', 'Death', 'Idle', 'Hit', 'Walk'];
-const redList = ['Magic', "lobber", "decoy", "player", "vampire"];
+const redList = ['Magic', "lobber", "decoy", "player"];
 fs.readdirSync('.').forEach(file => {
     if (onlyList.every(ol => file.includes(ol)) && greenList.some(gl => file.includes(gl)) && !redList.some(rl => file.includes(rl))) {
         console.log(file);
         // Modify
-        const magickCommand = `magick composite -gravity center ${file} ../shadow-priest.png ${file}`;
+        const magickCommand = `magick composite -gravity center ${file} ../shadow-vampire.png ${file}`;
         exec(magickCommand, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
