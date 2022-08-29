b6f81716212c2843a426d4d46e99e06c32daf2c6
Jordan O'Leary <jdoleary@gmail.com>
2022-08-29 11:23:18 -0400

    art: Add shadows for playerDeath

--- images/units/composite.js
+++ images/units/composite.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const { exec } = require("child_process");
 
-const onlyList = ['playerHit']
-const greenList = ['Attack', 'Death', 'Idle', 'Hit', 'Walk'];
+const onlyList = []
+const greenList = ['playerDeath_17', 'playerDeath_18', 'playerDeath_19', 'playerDeath_20', 'playerDeath_21', 'playerDeath_22'];
 const redList = ['Magic'];
 fs.readdirSync('.').forEach(file => {
     if (onlyList.every(ol => file.includes(ol)) && greenList.some(gl => file.includes(gl)) && !redList.some(rl => file.includes(rl))) {
         // Modify
-        const magickCommand = `magick composite -gravity center ${file} ../shadow-player-hit.png ${file}`;
+        const magickCommand = `magick composite -gravity center ${file} ../playerDeath_17-shadow.png ${file}`;
         exec(magickCommand, (error, stdout, stderr) => {
             console.log(file);
             if (error) {
