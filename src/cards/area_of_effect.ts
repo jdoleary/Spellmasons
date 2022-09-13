e38b76da7b1edf665ac435e746bf4592d63b2306
Jordan O'Leary <jdoleary@gmail.com>
2022-09-13 13:35:46 -0400

    art: Animate 'Expanding'

--- src/cards/area_of_effect.ts
+++ src/cards/area_of_effect.ts
@@ -2,6 +2,10 @@ import { addPickupTarget, addUnitTarget, Spell } from './index';
 import { drawPredictionCircle } from '../graphics/PlanningView';
 import { CardCategory } from '../types/commonTypes';
 import * as colors from '../graphics/ui/colors';
+import { raceTimeout } from '../Promise';
+import { Vec2 } from '../jmath/Vec';
+import Underworld from '../Underworld';
+import * as config from '../config';
 
 const id = 'Expanding';
 const range = 140;
@@ -39,6 +43,8 @@ Adds a radius to the spell so it can affect more targets.
           } else {
             drawPredictionCircle(target, adjustedRange, colors.targetingSpellGreen, 'Expand Radius');
           }
+        } else {
+          await animate(target, adjustedRange, underworld);
         }
         const withinRadius = underworld.getUnitsWithinDistanceOfTarget(
           target,
@@ -61,4 +67,52 @@ Adds a radius to the spell so it can affect more targets.
     },
   },
 };
+async function animate(pos: Vec2, radius: number, underworld: Underworld) {
+  const iterations = 100;
+  const millisBetweenIterations = 8;
+  // "iterations + 10" gives it a little extra time so it doesn't timeout right when the animation would finish on time
+  return raceTimeout(millisBetweenIterations * (iterations + 10), 'animatedExpand', new Promise<void>(resolve => {
+    for (let i = 0; i < iterations; i++) {
+
+      setTimeout(() => {
+        if (predictionGraphics) {
+          predictionGraphics.clear();
+          predictionGraphics.lineStyle(2, colors.targetingSpellGreen, 1.0)
+          predictionGraphics.beginFill(colors.targetingSpellGreen, 0.2);
+
+          const animatedRadius = radius * easeOutCubic((i + 1) / iterations)
+          predictionGraphics.drawCircle(pos.x, pos.y, animatedRadius);
+          predictionGraphics.endFill();
+          // Draw circles around new targets
+          let withinRadius: Vec2[] = [];
+          withinRadius = withinRadius.concat(
+            underworld.getUnitsWithinDistanceOfTarget(
+              pos,
+              animatedRadius,
+              false
+            ),
+            underworld.getPickupsWithinDistanceOfTarget(
+              pos,
+              animatedRadius,
+              false
+            ));
+          withinRadius.forEach(v => {
+            predictionGraphics?.drawCircle(v.x, v.y, config.COLLISION_MESH_RADIUS);
+          })
+        }
+        if (i >= iterations - 1) {
+          resolve();
+        }
+
+      }, millisBetweenIterations * i)
+    }
+  })).then(() => {
+    predictionGraphics?.clear();
+  });
+}
+// from https://easings.net/
+// input should be 0 - 1
+function easeOutCubic(x: number): number {
+  return 1 - Math.pow(1 - x, 3);
+}
 export default spell;
