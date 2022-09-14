08f9cf55f95f908db0c5d0ef355d1e66c780a94f
Jordan O'Leary <jdoleary@gmail.com>
2022-09-14 08:30:40 -0400

    fix: regression: Play one slash sfx for each hurt animation (but not for each unit, just for each group that plays simultaneously)

--- src/cards/hurt.ts
+++ src/cards/hurt.ts
@@ -36,21 +36,16 @@ Deals ${damageDone} damage to all targets.
       let animationDelaySum = 0;
       // .filter: only target living units
       const targets = state.targetedUnits.filter(u => u.alive)
-      if (targets.length) {
-        playDefaultSpellSFX(card, prediction);
-      }
-      for (let unit of targets) {
-        if (!unit) {
-          continue;
-        }
-        animationDelaySum = 0;
-        let delayBetweenAnimations = delayBetweenAnimationsStart;
-        // Note: quantity loop should always be INSIDE of the targetedUnits loop
-        // so that any quantity-based animations will play simultaneously on multiple targets
-        // but sequentially within themselves (on a single target, e.g. multiple hurts over and over)
-        for (let q = 0; q < quantity; q++) {
-          if (!prediction) {
-            setTimeout(() => {
+      animationDelaySum = 0;
+      let delayBetweenAnimations = delayBetweenAnimationsStart;
+      // Note: quantity loop should always be INSIDE of the targetedUnits loop
+      // so that any quantity-based animations will play simultaneously on multiple targets
+      // but sequentially within themselves (on a single target, e.g. multiple hurts over and over)
+      for (let q = 0; q < quantity; q++) {
+        if (!prediction) {
+          setTimeout(() => {
+            playDefaultSpellSFX(card, prediction);
+            for (let unit of targets) {
               const spellEffectImage = oneOffImage(unit, animationPath, containerSpells);
               if (spellEffectImage) {
                 // Randomize rotation a bit so that subsequent slashes don't perfectly overlap
@@ -63,13 +58,15 @@ Deals ${damageDone} damage to all targets.
               setTimeout(() => {
                 Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
               }, 100)
-            }, animationDelaySum)
-            animationDelaySum += delayBetweenAnimations;
-            // Don't let it go below 100 milliseconds
-            delayBetweenAnimations = Math.max(100, delayBetweenAnimations);
-            // Juice: Speed up subsequent hits
-            delayBetweenAnimations *= 0.80
-          } else {
+            }
+          }, animationDelaySum)
+          animationDelaySum += delayBetweenAnimations;
+          // Don't let it go below 100 milliseconds
+          delayBetweenAnimations = Math.max(100, delayBetweenAnimations);
+          // Juice: Speed up subsequent hits
+          delayBetweenAnimations *= 0.80
+        } else {
+          for (let unit of targets) {
             Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
           }
         }
