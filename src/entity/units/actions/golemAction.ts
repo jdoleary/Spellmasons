00f3867b4723f33136ef626dae87446a049dd093
Jordan O'Leary <jdoleary@gmail.com>
2022-10-10 10:19:53 -0400

    src: Add extra check to prevent (and report) false-negative melee predictions

--- src/entity/units/actions/golemAction.ts
+++ src/entity/units/actions/golemAction.ts
@@ -11,6 +11,8 @@ export async function action(unit: Unit.IUnit, attackTarget: Unit.IUnit | undefi
     // Do not move if they don't have a target
     return;
   }
+  // Calculate ahead of time to ensure they won't attack if the badge doesn't show
+  const precalculatedCanAttack = underworld.canUnitAttackTarget(unit, attackTarget);
   // Movement
   await Unit.moveTowards(unit, attackTarget, underworld);
 
@@ -21,9 +23,21 @@ export async function action(unit: Unit.IUnit, attackTarget: Unit.IUnit | undefi
   // Instead, just check that the distance is within the attack range
   // and let canAttackEnemy be used for just the attention markers
   if (withinMeleeRange(unit, attackTarget)) {
-    await Unit.playComboAnimation(unit, unit.animations.attack, async () =>
-      Unit.takeDamage(attackTarget, unit.damage, unit, underworld, false, undefined)
-    );
+    if (precalculatedCanAttack) {
+      await Unit.playComboAnimation(unit, unit.animations.attack, async () =>
+        Unit.takeDamage(attackTarget, unit.damage, unit, underworld, false, undefined)
+      );
+    } else {
+      // This check is extra to guard against false-negative melee attack predictions which should be solved in 21a5ea2a
+      // What happened was units were able to move into negative stamina if their remaining stamina was < 1 and moveDistance was greater than the 
+      // remaining stamina which allowed them to move closer than predicted which under some circumstances allowed them to attack without 
+      // having an attack badge above their heads.  I believe this is fully resolved in 21a5ea2a; however, precalculatingCanAttack before movement
+      // is an extra safety to prevent this from happening again (though it must be applied to EVERY melee unit individually in their action() function).
+      // It works by checking if they would have an attackBadge before movement, then after movement it wont let them attack even if they are in range.
+      // In that case, it will hit this else block and report the error.  I suspect to never see this error logged in monitoring, but it's here just
+      // in case to prevent the false-negative (which could ruin a run for a player and is super unfair.) 
+      console.error('Melee prediction was incorrect!', unit.stamina, `${unit.x}, ${unit.y}`, `${attackTarget.x},${attackTarget.y}`, unit.attackRange)
+    }
   }
 }
 export function withinMeleeRange(unit: Unit.IUnit, target: Vec2): boolean {
