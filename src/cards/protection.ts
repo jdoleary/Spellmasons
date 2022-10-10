eb07c6d0c0605b4337f8d906bcfc09153fbf53ae
Jordan O'Leary <jdoleary@gmail.com>
2022-10-10 13:51:37 -0400

    content: Readd 'protection'

--- src/cards/protection.ts
+++ src/cards/protection.ts
@@ -11,8 +11,8 @@ const spell: Spell = {
     healthCost: 0,
     expenseScaling: 1,
     probability: 10,
-    thumbnail: 'protection.png',
-    description: 'Protects yourself and allies from being effected by the spell you are about to cast.  Will protect 1 allied unit (including self) per use.  Prioritizes protecting yourself, then ally wizards, then other allies.  Multiple stacks of this spell will protect multiple people.',
+    thumbnail: 'unknown.png',
+    description: 'Protects yourself and allies from being effected by the spell you are about to cast.  Will protect 1 allied unit (including self) per use.  Prioritizes protecting yourself, then ally wizards, then other allies.  Multiple stacks of this spell will protect multiple people.  This spell must be cast AFTER an ally is targeted by a previous spell.',
     effect: async (state, card, quantity, underworld, prediction) => {
       const allies = [
         // Prioritize self over all other allies
