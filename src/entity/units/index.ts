457c04bb37cf1fc5b32a957e8faab630b9e5e646
Jordan O'Leary <jdoleary@gmail.com>
2022-10-15 16:56:16 -0400

    content: Temporarily remove dragger
    until it's kinks are sorted out.
    It's attack needs work:
    - Dragger had intent icon but moved instead of attacking

--- src/entity/units/index.ts
+++ src/entity/units/index.ts
@@ -50,7 +50,7 @@ export function registerUnits() {
   register(priest);
   register(poisoner);
   register(vampire);
-  register(dragger);
+  // register(dragger);
 
   register(playerUnit);
   register(decoy);
