b8142b31e16f5f56659086af309b37fd7db74112
Jordan O'Leary <jdoleary@gmail.com>
2022-10-27 17:01:39 -0400

    fix: Server should process all loops
    immediately.  Otherwise there will be built in lag
    where all the clients are waiting for the server to process
    something in real time and nothing is happening
    on their screens.
    See GameLoops.md for more information.
    The headless server gameloops are carefully designed to execute
     completely when needed and then to exit.

--- src/Shims.ts
+++ src/Shims.ts
@@ -19,16 +19,12 @@ global.document = {
     }
 };
 globalThis.addEventListener = function () { };
-const TICK_RATE = 0;
-console.log('TICK_RATE set to', TICK_RATE)
 
 global.cancelAnimationFrame = (id) => {
     clearTimeout(id);
 }
 global.requestAnimationFrame = (callback) => {
-    // Note, changing TICK_RATE from undefined to 16
-    // went from consuming 20% cpu to 7% cpu
-    return setTimeout(() => callback(Date.now()), TICK_RATE);
+    return callback(Date.now());
 }
 // Headless server plays no audio
 global.playSFX = (_string) => { };
