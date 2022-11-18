e87160eaef5da073738df421bd83228f4ffc2075
Jordan O'Leary <jdoleary@gmail.com>
2022-11-18 08:49:17 -0500

    art: Improve light-beam so that it is a solid beam and doesn't fade off

--- src/VisualEffects.ts
+++ src/VisualEffects.ts
@@ -10,30 +10,33 @@ export function skyBeam(position: Vec2) {
     const whiteCircle = addPixiSprite('half-circle.png', containerProjectiles)
     if (whiteCircle) {
         whiteCircle.x = position.x;
-        whiteCircle.y = position.y;
+        // -1 ensures the white circle and light beam don't
+        // have a visible empty line between them.
+        whiteCircle.y = position.y - 1;
         whiteCircle.scale.y = 0.75;
         whiteCircle.anchor.set(0.5);
     }
 
-    const lightBeam = addPixiSprite('light-beam.png', containerProjectiles);
+    const lightBeam = addPixiSprite('light-beam.png', whiteCircle);
     if (lightBeam) {
         lightBeam.anchor.set(0.5);
         const beamHeight = 1000;
         lightBeam.scale.y = beamHeight;
-        lightBeam.x = position.x
-        lightBeam.y = position.y - beamHeight / 2;
+        lightBeam.x = 0;
+        lightBeam.y = - beamHeight / 2;
         const animateAway = () => {
-            lightBeam.alpha *= 0.97
             if (whiteCircle) {
-                whiteCircle.alpha = lightBeam.alpha / 2;
-            }
-            // Continue to animate until the alpha is low enought
-            if (lightBeam.alpha > 0.02) {
-                requestAnimationFrame(animateAway);
+                whiteCircle.alpha *= 0.97
+                // Continue to animate until the alpha is low enought
+                if (whiteCircle.alpha > 0.02) {
+                    requestAnimationFrame(animateAway);
+                } else {
+                    // When done, clean up assets
+                    lightBeam.parent.removeChild(lightBeam);
+                    whiteCircle.parent.removeChild(whiteCircle);
+                }
             } else {
-                // When done, clean up assets
                 lightBeam.parent.removeChild(lightBeam);
-                whiteCircle?.parent.removeChild(whiteCircle);
             }
         }
         animateAway();
