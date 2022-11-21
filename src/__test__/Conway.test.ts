d1fa25907eeed552824191775e9fb99d695d636d
Jordan O'Leary <jdoleary@gmail.com>
2022-11-21 15:42:11 -0500

    test: Improve test specificity
    to solve jest error:  If it should pass with deep equality, replace "toBe" with "toStrictEqual"

--- src/__test__/Conway.test.ts
+++ src/__test__/Conway.test.ts
@@ -4,7 +4,7 @@ describe('getNeighborIndices', () => {
     it('should return the neighboring indicies around the given index', () => {
         const actual = getNeighborIndices(4, 3);
         const expected = [0, 1, 2, 5, 8, 7, 6, 3];
-        expect(actual).toBe(expected);
+        expect(actual).toEqual(expected);
 
     });
 });
