83c68a7a17859fb6d498cf1e71aaf16302f74f76
Jordan O'Leary <jdoleary@gmail.com>
2022-12-19 11:35:02 -0500

    art: animate rend

--- src/cards/rend.ts
+++ src/cards/rend.ts
@@ -4,6 +4,9 @@ import { CardCategory } from '../types/commonTypes';
 import { oneOffImage, playDefaultSpellSFX } from './cardUtils';
 import { Spell } from './index';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
+import * as Image from '../graphics/Image';
+import { clone, Vec2 } from '../jmath/Vec';
+import { raceTimeout } from '../Promise';
 
 export const id = 'Rend';
 function calculateRendDamage(stack: number): number {
@@ -21,6 +24,7 @@ export interface UnitDamage {
   damageTaken: number;
 
 }
+const spellRendAnimationHeight = 10;
 const animationPath = 'spell-effects/spellRend';
 const spell: Spell = {
   card: {
@@ -38,29 +42,56 @@ const spell: Spell = {
 Deals more damage based on the number of consecutive stacks of "${id}".
 How damage grows when stacking ${id}: ${calculateRendDamage(1)}, ${calculateRendDamage(2)}, ${calculateRendDamage(3)}, ${calculateRendDamage(4)}, ${calculateRendDamage(5)}, ${calculateRendDamage(6)}, ${calculateRendDamage(7)}, ${calculateRendDamage(8)}, ${calculateRendDamage(9)}, ${calculateRendDamage(10)}`,
     effect: async (state, card, quantity, underworld, prediction) => {
-      await new Promise<void>((resolve) => {
-        const damage = calculateRendDamage(quantity);
-        // .filter: only target living units
-        const targets = state.targetedUnits.filter(u => u.alive)
-        if (!prediction) {
-          playDefaultSpellSFX(card, prediction);
-          for (let unit of targets) {
-            const spellEffectImage = oneOffImage(unit, animationPath, containerSpells, resolve);
-            if (spellEffectImage) {
-              spellEffectImage.sprite.scale.x = quantity / 6;
-              spellEffectImage.sprite.scale.y = quantity / 6;
-            }
-            Unit.takeDamage(unit, damage, state.casterUnit, underworld, prediction, state);
-          }
-        } else {
-          for (let unit of targets) {
-            Unit.takeDamage(unit, damage, state.casterUnit, underworld, prediction, state);
-          }
-          resolve();
-        }
-      });
+      const damage = calculateRendDamage(quantity);
+      // .filter: only target living units
+      const targets = state.targetedUnits.filter(u => u.alive)
+      if (!prediction) {
+        playDefaultSpellSFX(card, prediction);
+      }
+      await animateRend(targets, quantity, prediction);
+      for (let unit of targets) {
+        Unit.takeDamage(unit, damage, state.casterUnit, underworld, prediction, state);
+      }
       return state;
     },
   },
 };
 export default spell;
+function animateRend(targets: Vec2[], quantity: number, prediction: boolean): Promise<void> {
+  if (!prediction) {
+    return raceTimeout(500 + 120 * quantity, 'animateRend',
+      new Promise<void>((resolve) => {
+        for (let unit of targets) {
+          // For rend effect only, make more instances going upward for each quantity
+          for (let q = 0; q < quantity; q++) {
+            const isLastAnimatedInstance = q == quantity - 1;
+            setTimeout(() => {
+              const image = Image.create(
+                { x: unit.x, y: unit.y - ((quantity - 1) * spellRendAnimationHeight / 2) + q * spellRendAnimationHeight },
+                animationPath,
+                containerSpells,
+                {
+                  loop: false,
+                  animationSpeed: 0.3,
+                  onComplete: () => {
+                    Image.hide(image)
+                    Image.cleanup(image);
+                    if (isLastAnimatedInstance) {
+
+                      resolve()
+                    }
+                  }
+                }
+              );
+              if (image && isLastAnimatedInstance) {
+                image.resolver = resolve;
+              }
+            }, 100 * q);
+          }
+        }
+      }));
+  } else {
+    return Promise.resolve();
+  }
+
+}
