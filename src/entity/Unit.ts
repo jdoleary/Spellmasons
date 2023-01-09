ba5c12ec8bb02e7fb025e9ae20c24099b779f0b0
Jordan O'Leary <jdoleary@gmail.com>
2023-01-09 09:51:53 -0500

    ref: Improve updating own player unit bars when
    takeDamage

--- src/entity/Unit.ts
+++ src/entity/Unit.ts
@@ -716,7 +716,7 @@ export function takeDamage(unit: IUnit, amount: number, damageFromVec2: Vec2 | u
     die(unit, underworld, prediction);
   }
 
-  if (unit.unitSourceId == spellmasonUnitId && !prediction) {
+  if (unit.id == globalThis.player?.unit.id && !prediction) {
     // Now that the player unit's properties have changed, sync the new
     // state with the player's predictionUnit so it is properly
     // refelcted in the bar
