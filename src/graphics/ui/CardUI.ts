e60c244d3f522f770991bfde6202be3a4cc6bbd8
Jordan O'Leary <jdoleary@gmail.com>
2023-01-17 13:16:28 -0500

    UX: Allow for dragging spells off the toolbar

--- src/graphics/ui/CardUI.ts
+++ src/graphics/ui/CardUI.ts
@@ -62,6 +62,35 @@ const dragstart = (ev: any) => {
   }
 
 }
+const deleteCardFromSlot = (ev: any, overworld: Overworld) => {
+  const startDragCardIndex = getStartDragCardIndex();
+  if (startDragCardIndex !== -1) {
+    if (globalThis.player) {
+      console.log('UI: delete card from slot', startDragCardIndex);
+      globalThis.player.cards[startDragCardIndex] = '';
+      if (overworld.underworld) {
+        recalcPositionForCards(globalThis.player, overworld.underworld);
+        syncInventory(undefined, overworld.underworld);
+      } else {
+        console.error('Cannot drop card on toolbar, underworld is undefined.');
+      }
+      ev.preventDefault();
+    }
+  }
+
+}
+function getStartDragCardIndex(): number {
+  if (!dragCard) {
+    return -1;
+  }
+  let startDragCardIndex = dragCard.parentNode && (dragCard.closest('#card-hand') || dragCard.closest('.floating-card-holder')) ? Array.from(dragCard.parentNode.children).indexOf(dragCard) : -1;
+  const containerIndexOffset = cardContainers.indexOf(dragCard.parentNode as HTMLElement);
+  // Change startDragCardIndex based on which card container it originated from
+  if (containerIndexOffset !== -1) {
+    startDragCardIndex += NUMBER_OF_TOOLBAR_SLOTS * containerIndexOffset;
+  }
+  return startDragCardIndex;
+}
 const drop = (ev: any, overworld: Overworld, startIndex: number) => {
   const dropElement = ((ev.target as HTMLElement).closest('.slot') as HTMLElement);
   if (!dropElement) {
@@ -71,12 +100,7 @@ const drop = (ev: any, overworld: Overworld, startIndex: number) => {
   const dropIndex = startIndex + (dropElement.parentNode ? Array.from(dropElement.parentNode.children).indexOf(dropElement) : -1);
   const cardId = dragCard && dragCard.dataset.cardId
   if (globalThis.player && dropIndex !== -1 && dragCard && cardId !== undefined) {
-    let startDragCardIndex = dragCard.parentNode && (dragCard.closest('#card-hand') || dragCard.closest('.floating-card-holder')) ? Array.from(dragCard.parentNode.children).indexOf(dragCard) : -1;
-    const containerIndexOffset = cardContainers.indexOf(dragCard.parentNode as HTMLElement);
-    // Change startDragCardIndex based on which card container it originated from
-    if (containerIndexOffset !== -1) {
-      startDragCardIndex += NUMBER_OF_TOOLBAR_SLOTS * containerIndexOffset;
-    }
+    const startDragCardIndex = getStartDragCardIndex();
     if (startDragCardIndex !== -1) {
       // Then the drag card is already in the toolbar and this is a swap between
       // two cards on the toolbar
@@ -131,6 +155,9 @@ export function setupCardUIEventListeners(overworld: Overworld) {
         container.addEventListener('dragstart', dragstart);
         container.addEventListener('dragover', ev => {
           ev.preventDefault();
+        });
+        container.addEventListener('dragend', ev => {
+          deleteCardFromSlot(ev, overworld);
         });
         container.addEventListener('drop', ev => drop(ev, overworld, (NUMBER_OF_TOOLBAR_SLOTS) * i));
         addCardInspectHandlers(container);
