945b3e6619654201ce985eab554719dec576ffd9
Jordan O'Leary <jdoleary@gmail.com>
2023-10-09 08:34:50 -0400

    balance: Immune units can still attack

--- src/cards/immune.ts
+++ src/cards/immune.ts
@@ -3,7 +3,6 @@ import * as Image from '../graphics/Image';
 import * as Unit from '../entity/Unit'
 import Underworld from '../Underworld';
 import { getOrInitModifier } from './util';
-import { summoningSicknessId } from '../modifierSummoningSickness';
 import { Vec2, equal } from '../jmath/Vec';
 import floatingText from '../graphics/FloatingText';
 import type { Sprite } from 'pixi.js';
@@ -13,22 +12,19 @@ export default function registerImmune() {
   registerModifiers(id, {
     init,
     add,
+    remove
   });
 }
 const imageName = 'spell-effects/modifierShield.png';
 export function init(unit: Unit.IUnit, underworld: Underworld, prediction: boolean) {
-  // Immune units miss a turn
-  Unit.addModifier(unit, summoningSicknessId, underworld, false);
-  // Stop the animation
-  unit.image?.sprite.stop();
   // Add subsprite image
   // @ts-ignore: imagePath is a property that i've added and is not a part of the PIXI type
   // which is used for identifying the sprite or animation that is currently active
-  const animatedFortifySprite = unit.image?.sprite.children.find(c => c.imagePath == imageName)
+  const animatedSprite = unit.image?.sprite.children.find(c => c.imagePath == imageName)
     || Image.addSubSprite(unit.image, imageName);
-  if (animatedFortifySprite) {
+  if (animatedSprite) {
     // Make it red
-    (animatedFortifySprite as Sprite).tint = 0xff0000;
+    (animatedSprite as Sprite).tint = 0xff0000;
   }
 }
 export function add(unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) {
@@ -36,6 +32,10 @@ export function add(unit: Unit.IUnit, underworld: Underworld, prediction: boolea
     init(unit, underworld, prediction)
   });
 }
+export function remove(unit: Unit.IUnit, underworld: Underworld) {
+  Image.removeSubSprite(unit.image, imageName);
+}
+
 let notified: Vec2[] = [];
 export function resetNotifiedImmune() {
   notified = [];
