02ee796eb1f224ff02577ddeeb5c6240821fb737
Jordan O'Leary <jdoleary@gmail.com>
2023-11-11 22:48:52 -0500

    fix: multiplayer menu bug where when the game restarts, it reset player.isReady so it was showing the wrong menu on esc

--- src/Underworld.ts
+++ src/Underworld.ts
@@ -2006,7 +2006,9 @@ ${CardUI.cardListToImages(player.stats.longestSpell)}
           this.isRestarting = setTimeout(() => {
             const newUnderworld = new Underworld(overworld, pie, Math.random().toString());
             // Add players back to underworld
-            ensureAllClientsHaveAssociatedPlayers(overworld, overworld.clients)
+            ensureAllClientsHaveAssociatedPlayers(overworld, overworld.clients);
+            // Since they are still in the game, set them to lobbyReady
+            newUnderworld.players.filter(p => p.clientConnected).forEach(p => { p.lobbyReady = true; });
             // Generate the level data
             newUnderworld.lastLevelCreated = newUnderworld.generateLevelDataSyncronous(0, this.gameMode);
             // Actually create the level 
