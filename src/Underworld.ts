c0485ddb7d3ac4fd9cd40f1296894aaa862dd854
Jordan O'Leary <jdoleary@gmail.com>
2023-11-29 08:21:13 -0500

    lint

--- src/Underworld.ts
+++ src/Underworld.ts
@@ -1232,7 +1232,7 @@ export default class Underworld {
   spawnPickup(index: number, coords: Vec2, prediction?: boolean) {
     const pickup = Pickup.pickups[index];
     if (pickup) {
-      Pickup.create({ pos: coords, pickupSource: pickup, logSource:'spawnPickup' }, this, !!prediction);
+      Pickup.create({ pos: coords, pickupSource: pickup, logSource: 'spawnPickup' }, this, !!prediction);
     } else {
       console.error('Could not find pickup with index', index);
     }
@@ -1854,7 +1854,7 @@ export default class Underworld {
     for (let p of pickups) {
       const pickup = Pickup.pickups[p.index];
       if (pickup) {
-        Pickup.create({ pos: p.coord, pickupSource: pickup, logSource:'createLevelSyncronous' }, this, false);
+        Pickup.create({ pos: p.coord, pickupSource: pickup, logSource: 'createLevelSyncronous' }, this, false);
       } else {
         console.error('Could not find pickup source with index', p.index);
       }
@@ -3596,7 +3596,7 @@ ${CardUI.cardListToImages(player.stats.longestSpell)}
           for (let playerUnit of this.units.filter(u => u.unitType == UnitType.PLAYER_CONTROLLED && u.alive)) {
             const portalSpawnLocation = this.findValidSpawn(playerUnit, 4) || playerUnit;
             if (!isOutOfBounds(portalSpawnLocation, this)) {
-              Pickup.create({ pos: portalSpawnLocation, pickupSource: portalPickup, logSource:'Portal' }, this, false);
+              Pickup.create({ pos: portalSpawnLocation, pickupSource: portalPickup, logSource: 'Portal' }, this, false);
             } else {
               // If a pickup is attempted to be spawned for an unspawned player this if check prevents it from being spawned out of bounds
               // and the next invokation of checkIfShouldSpawnPortal will spawn it instead.
@@ -3745,7 +3745,7 @@ ${CardUI.cardListToImages(player.stats.longestSpell)}
   syncPickups(pickups: Pickup.IPickupSerialized[]) {
     // Remove pickups flagged for removal before syncing
     this.pickups = this.pickups.filter(p => !p.flaggedForRemoval);
-    console.log('sync: Syncing pickups', pickups.map(u => u.id), 'current pickups:', this.pickups.map(u => u.id));
+    console.log('sync: Syncing pickups', pickups.map(u => `${u.id}:${u.flaggedForRemoval}`), 'current pickups:', this.pickups.map(u => `${u.id}:${u.flaggedForRemoval}`));
 
     // Remove excess pickups if local copy of pickups has more pickups than the pickups it
     // should be syncing with
@@ -3914,7 +3914,7 @@ ${CardUI.cardListToImages(player.stats.longestSpell)}
   // hash() {
   //   const state = this.serializeForHash();
   //   const hashResult = hash(state);
-  //   console.log(`hash-${hashResult}:`, JSON.stringify(state));
+  //   console.log(`hash - ${ hashResult }: `, JSON.stringify(state));
   //   return hashResult
   // }
   // Returns a modified copy of gamestate that is used when generating a hash
@@ -4014,7 +4014,7 @@ export type IUnderworldSerializedForSyncronize = Omit<Pick<Underworld, Underworl
 //       }
 //       return sums;
 //     }, {})
-//     console.log(`level ${i}`, sums);
+//     console.log(`level ${ i }`, sums);
 //   }
 //   console.log('New:')
 //   for (let i = 0; i < 10; i++) {
@@ -4027,7 +4027,7 @@ export type IUnderworldSerializedForSyncronize = Omit<Pick<Underworld, Underworl
 //       }
 //       return sums;
 //     }, {})
-//     console.log(`level ${i}`, sums);
+//     console.log(`level ${ i }`, sums);
 //   }
 // }
 
