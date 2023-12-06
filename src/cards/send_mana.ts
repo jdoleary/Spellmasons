1c168ab9da577172513eed4dc1db29a059c8b8c3
Jamison Perkins <jamisonperkins@gmail.com>
2023-12-06 14:34:11 -0600

    Improved Send-Mana fx
    Now uses mana_steal animation and potion sound effects

--- src/cards/send_mana.ts
+++ src/cards/send_mana.ts
@@ -5,6 +5,12 @@ import { CardCategory } from '../types/commonTypes';
 import { playDefaultSpellSFX } from './cardUtils';
 import { Spell } from './index';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
+import { makeManaTrail } from '../graphics/Particles';
+import { addPixiSpriteAnimated } from '../graphics/PixiUtils';
+import { manaBlue } from '../graphics/ui/colors';
+import { MultiColorReplaceFilter } from '@pixi/filter-multi-color-replace';
+import * as config from '../config';
+import { explain, EXPLAIN_OVERFILL } from '../graphics/Explain';
 
 export const id = 'send_mana';
 const amount = 20;
@@ -12,8 +18,8 @@ const amount = 20;
 const spell: Spell = {
   card: {
     id: id,
-    category: CardCategory.Blessings,
-    sfx: 'heal', // TODO - Different sfx?
+    category: CardCategory.Mana,
+    sfx: 'potionPickupMana',
     supportQuantity: true,
     manaCost: amount,
     healthCost: 0,
@@ -23,25 +29,65 @@ const spell: Spell = {
     animationPath: 'spell-effects/potionPickup',
     description: ['spell_send_mana', amount.toString()], //TODO - Mana descr
     effect: async (state, card, quantity, underworld, prediction) => {
-      // Since all targets are animated simultaneously we only need to await
-      // the latest one, no need to use Promise.all
-      let animationPromise = Promise.resolve();
-      // .filter: only target living units
-      for (let unit of state.targetedUnits.filter(u => u.alive)) {
-        const manaToRestore = amount * quantity;
-        if (!prediction && quantity > 1) {
-          for (let unit of state.targetedUnits) {
-            floatingText({
-              coords: unit,
-              text: `+${Math.abs(manaToRestore)} Mana`
-            });
+      const targets = state.targetedUnits.filter(u => u.alive);
+      let promises = [];
+      for (let unit of targets) {
+        unit.mana += amount;
+        const manaTrailPromises = [];
+        if (!prediction) {
+          for (let i = 0; i < quantity; i++) {
+            manaTrailPromises.push(makeManaTrail(state.casterUnit, unit, underworld, '#e4f9ff', '#3fcbff'));
           }
         }
-        playDefaultSpellSFX(card, prediction);
-        unit.mana += manaToRestore;
-        animationPromise = Image.addOneOffAnimation(unit, 'spell-effects/potionPickup', {}, { loop: false, animationSpeed: 0.3 }); //TODO - Different animation?
+        promises.push((prediction ? Promise.resolve() : Promise.all(manaTrailPromises)));
       }
-      await animationPromise;
+      await Promise.all(promises).then(() => {
+        if (!prediction) {
+          playDefaultSpellSFX(card, prediction);
+          // Animate
+          for (let unit of targets) {
+            if (unit.image) {
+              // Note: This uses the lower-level addPixiSpriteAnimated directly so that it can get a reference to the sprite
+              // and add a filter; however, addOneOffAnimation is the higher level and more common for adding a simple
+              // "one off" animated sprite.  Use it instead of addPixiSpriteAnimated unless you need more direct control like
+              // we do here
+              const animationSprite = addPixiSpriteAnimated('spell-effects/potionPickup', unit.image.sprite, {
+                loop: false,
+                onComplete: () => {
+                  if (animationSprite?.parent) {
+                    animationSprite.parent.removeChild(animationSprite);
+                  }
+                }
+              });
+              if (animationSprite) {
+
+                if (!animationSprite.filters) {
+                  animationSprite.filters = [];
+                }
+                // Change the health color to blue
+                animationSprite.filters.push(
+                  new MultiColorReplaceFilter(
+                    [
+                      [0xff0000, manaBlue],
+                    ],
+                    0.1
+                  )
+                );
+              }
+            }
+
+            const finalManaSent = amount * quantity;
+            unit.mana += finalManaSent;
+            floatingText({
+              coords: unit,
+              text: `+ ${finalManaSent} Mana`,
+              style: { fill: 'blue', ...config.PIXI_TEXT_DROP_SHADOW }
+            });
+          }
+          explain(EXPLAIN_OVERFILL);
+        }
+      });
+      //refund if no targets ?
       return state;
     },
   },
