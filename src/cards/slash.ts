35e61229b5bf848e5eeee8fffc12ff4e3c6af361
Jordan O'Leary <jdoleary@gmail.com>
2023-12-18 15:05:43 -0500

    fix: desync in slash
    Where slash would return before all damage
    was done being dished out.
    
    Resolves #166

--- src/cards/slash.ts
+++ src/cards/slash.ts
@@ -37,28 +37,32 @@ const spell: Spell = {
       const targets = state.targetedUnits.filter(u => u.alive)
       animationDelaySum = 0;
       let delayBetweenAnimations = delayBetweenAnimationsStart;
+      let attackPromises = [];
       // Note: quantity loop should always be INSIDE of the targetedUnits loop
       // so that any quantity-based animations will play simultaneously on multiple targets
       // but sequentially within themselves (on a single target, e.g. multiple hurts over and over)
       for (let q = 0; q < quantity; q++) {
         if (!prediction && !globalThis.headless) {
-          setTimeout(() => {
-            playDefaultSpellSFX(card, prediction);
-            for (let unit of targets) {
-              const spellEffectImage = oneOffImage(unit, animationPath, containerSpells);
-              if (spellEffectImage) {
-                // Randomize rotation a bit so that subsequent slashes don't perfectly overlap
-                spellEffectImage.sprite.rotation = randFloat(-Math.PI / 6, Math.PI / 6);
-                if (q % 2 == 0) {
-                  // Flip every other slash animation so that it comes from the other side
-                  spellEffectImage.sprite.scale.x = -1;
+          attackPromises.push(new Promise<void>((resolve) => {
+            setTimeout(() => {
+              playDefaultSpellSFX(card, prediction);
+              for (let unit of targets) {
+                const spellEffectImage = oneOffImage(unit, animationPath, containerSpells);
+                if (spellEffectImage) {
+                  // Randomize rotation a bit so that subsequent slashes don't perfectly overlap
+                  spellEffectImage.sprite.rotation = randFloat(-Math.PI / 6, Math.PI / 6);
+                  if (q % 2 == 0) {
+                    // Flip every other slash animation so that it comes from the other side
+                    spellEffectImage.sprite.scale.x = -1;
+                  }
                 }
+                setTimeout(() => {
+                  Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
+                  resolve();
+                }, 100);
               }
-              setTimeout(() => {
-                Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
-              }, 100)
-            }
-          }, animationDelaySum)
+            }, animationDelaySum);
+          }));
           animationDelaySum += delayBetweenAnimations;
           // Don't let it go below 20 milliseconds
           delayBetweenAnimations = Math.max(20, delayBetweenAnimations);
@@ -74,9 +78,7 @@ const spell: Spell = {
         refundLastSpell(state, prediction, 'No valid targets. Cost refunded.');
       }
       if (!prediction && !globalThis.headless) {
-        await new Promise((resolve) => {
-          setTimeout(resolve, animationDelaySum);
-        })
+        await Promise.all(attackPromises);
       }
       return state;
     },
