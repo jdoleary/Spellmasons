7e623371f4088dec6424c95ce65cf484d5cc63d0
Jordan O'Leary <jdoleary@gmail.com>
2023-12-29 08:25:38 -0600

    ref: Rebuilt sync function to return object arrays with sync instructions so it's testible

--- src/Syncronization.ts
+++ src/Syncronization.ts
@@ -3,17 +3,59 @@ import { UnitType } from "./types/commonTypes";
 
 // syncUnits should:
 // NOT remove any existing player's units (even if there are discrepancies)
-// Sync the state of player units
-// Sync the state of identical non player units
-// Remove units not in the serialized units array (so long as they are not player units)
+//   Note: Missing player unit's should be sent to the server from clients
+//   This should piggy back on CLIENT_SEND_PLAYER_TO_SERVER
+// Sync the state of identityMatched units (same id, same type, same sourceId)
+// Remove current units not in the serialized units array (so long as they are not player units)
 // Create missing units from the serialized array (careful not to overwrite ids of existing units)
-interface UnitSubType {
-    id: number;
-    unitSourceId: string;
-    unitType: UnitType;
+
+// TODO: INPROGRESS (replace `any`s) maybe use a subtype so it's testable without constructing a whole underworld
+interface syncFunctionReturn {
+    // Which objects should be synced,
+    // first is from `current`
+    // last is from `syncFrom`
+    sync: [any, any][];
+    //  current to remove
+    remove: any[];
+    //  Current objects to send to the server
+    syncToServer: any[];
+    // Objects of syncFrom to create new
+    create: any[];
+
 }
-export function syncUnits(current: Unit.IUnit[], syncFrom: Unit.IUnitSerialized[]): Unit.IUnit[] {
-    // TODO maybe use a subtype so it's testable without constructing a whole underworld
-    return [];
+// Identity match determines if the units are the same entity (and then it's okay to sync from syncFrom to current for that entity)
+export function getSyncActions(
+    current: any[],
+    syncFrom: any[],
+    findMatchIndex: (a: any, potentialMatches: any[]) => number,
+    ignoreSync: (a: any) => boolean): syncFunctionReturn {
+    const ret: syncFunctionReturn = {
+        sync: [],
+        remove: [],
+        syncToServer: [],
+        create: []
+    }
+    const matches: any[] = []
+    for (let i = 0; i < current.length; i++) {
+        const e = current[i];
+        const matchIndex = findMatchIndex(e, syncFrom);
+        const match = syncFrom[matchIndex]
+        if (match) {
+            // Keep track of what synced so we can create the extras in syncFrom as new
+            matches.push(match);
+            ret.sync.push([e, match]);
+        } else {
+            if (!ignoreSync(e)) {
+                ret.remove.push(e);
+            } else {
+                ret.syncToServer.push(e);
+            }
+        }
+    }
+    const toBeCreated = syncFrom.filter(x => !matches.includes(x));
+    for (let e of toBeCreated) {
+        ret.create.push(e);
+    }
+    return ret;
 
 }
--- src/__test__/Syncronization.test.ts
+++ src/__test__/Syncronization.test.ts
@@ -2,51 +2,47 @@ import * as Unit from "../entity/Unit"
 import playerUnit from "../entity/units/playerUnit";
 import { Faction, UnitSubType, UnitType } from "../types/commonTypes";
 import golem from '../entity/units/golem';
-import { syncUnits } from '../Syncronization';
+import { getSyncActions } from '../Syncronization';
 import { registerUnit } from "../entity/units";
-function makePlayerUnit(): Unit.IUnit {
-    return Unit.create(
-        playerUnit.id,
-        // x,y of NaN denotes that the player unit is
-        // inPortal.  See the function inPortal for more
-        NaN,
-        NaN,
-        Faction.ALLY,
-        playerUnit.info.image,
-        UnitType.PLAYER_CONTROLLED,
-        playerUnit.info.subtype,
-        undefined,
-        // @ts-ignore no underworld in tests
-        undefined
-    );
-}
-function makeGolem(): Unit.IUnit {
-    const sourceUnit = golem;
-    return Unit.create(
-        sourceUnit.id,
-        0,
-        0,
-        Faction.ENEMY,
-        sourceUnit.info.image,
-        UnitType.AI,
-        sourceUnit.info.subtype,
-        sourceUnit.unitProps,
-        // @ts-ignore no underworld in tests
-        undefined
-    );
 
+interface testSyncObject {
+    id: number;
+    type: string;
+    value: number;
 }
-registerUnit(playerUnit);
-registerUnit(golem);
+function findMatchIndex(current: testSyncObject, potentialMatches: testSyncObject[]): number {
+    return potentialMatches.findIndex(x => x.id == current.id);
+}
+function sync(current: testSyncObject, from: testSyncObject) {
+    current.type = from.type;
+    current.value = from.value;
+}
+function ignoreSync(current: testSyncObject): boolean {
+    return current.type == 'player';
+}
+function makeTestSyncObject(id: number, type: string, value: number): testSyncObject {
+    return {
+        id,
+        type,
+        value
+    }
+}
+
 describe('Syncronization', () => {
     it('should NOT remove any existing player units', () => {
-        const playerUnit1 = makePlayerUnit();
-        const golem = makeGolem();
-        const golem2 = makeGolem();
-        golem2.id = 3;
-        const actual = syncUnits([playerUnit1, golem], [Unit.serialize(golem2)]);
-        expect(actual).toEqual([playerUnit1, golem2]);
-
-
+        const current = [
+            makeTestSyncObject(0, 'player', 1),
+            makeTestSyncObject(1, 'unit', 10),
+        ];
+        const from = [
+            makeTestSyncObject(1, 'unit', 11),
+        ];
+        const actual = getSyncActions(current, from, findMatchIndex, ignoreSync);
+        expect(actual).toEqual({
+            sync: [[current[1], from[0]]],
+            remove: [],
+            syncToServer: [current[0]],
+            create: []
+        });
     });
 });
