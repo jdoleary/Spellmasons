8b3f5301302fe08e748750811f0343ee23edc780
Jamison Perkins <jamisonperkins@gmail.com>
2024-01-10 11:39:26 -0600

    implemented teleport effect

--- src/cards/displace.ts
+++ src/cards/displace.ts
@@ -2,17 +2,15 @@ import * as Unit from '../entity/Unit';
 import * as Pickup from '../entity/Pickup';
 import { getCurrentTargets, Spell } from './index';
 import type { Vec2 } from '../jmath/Vec';
-import * as Obstacle from '../entity/Obstacle';
 import { CardCategory } from '../types/commonTypes';
-import { skyBeam } from '../VisualEffects';
 import { playDefaultSpellSFX } from './cardUtils';
 import { HasSpace } from '../entity/Type';
 import Underworld from '../Underworld';
 import seedrandom from 'seedrandom';
-import * as colors from '../graphics/ui/colors';
 import { prng } from '../jmath/rand';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
 import * as config from '../config';
+import { teleport } from '../effects/teleport';
 
 export function findRandomDisplaceLocation(underworld: Underworld, radius: number, seed: prng): Vec2 | undefined {
   let isValid = false;
@@ -54,41 +52,15 @@ const spell: Spell = {
       for (let i = 0; i < quantity; i++) {
         // Loop through all targets and batch swap locations
         const swaps: [HasSpace, Vec2][] = [];
-        for (let targetUnit of targets) {
-          if (targetUnit) {
+        for (let targetObject of targets) {
+          if (targetObject) {
             const displaceLocation = findRandomDisplaceLocation(underworld, config.COLLISION_MESH_RADIUS, seed);
-            swaps.push([targetUnit, displaceLocation || targetUnit]);
+            swaps.push([targetObject, displaceLocation || targetObject]);
           }
         }
 
-        for (let [entity, newLocation] of swaps) {
-          if (!prediction) {
-            // Animate effect of unit spawning from the sky
-            skyBeam(newLocation);
-          }
-
-          // Show prediction lines before the move actually occurs
-          if (prediction && globalThis.predictionGraphics) {
-            globalThis.predictionGraphics.lineStyle(4, colors.forceMoveColor, 1.0);
-            globalThis.predictionGraphics.moveTo(entity.x, entity.y);
-            globalThis.predictionGraphics.lineTo(newLocation.x, newLocation.y);
-            // Draw circle at the end so the line path isn't a trail of rectangles with sharp edges
-            globalThis.predictionGraphics.lineStyle(1, colors.forceMoveColor, 1.0);
-            globalThis.predictionGraphics.beginFill(colors.forceMoveColor);
-            globalThis.predictionGraphics.drawCircle(newLocation.x, newLocation.y, 3);
-            globalThis.predictionGraphics.endFill();
-          }
-          if (Unit.isUnit(entity)) {
-            // Physically swap
-            Unit.setLocation(entity, newLocation);
-            // Check to see if unit interacts with liquid
-            Obstacle.tryFallInOutOfLiquid(entity, underworld, prediction);
-          } else if (Pickup.isPickup(entity)) {
-            Pickup.setPosition(entity, newLocation.x, newLocation.y);
-          } else {
-            entity.x = newLocation.x;
-            entity.y = newLocation.y;
-          }
+        for (let [object, newLocation] of swaps) {
+          teleport(object, newLocation, underworld, prediction, true);
         }
       }
       return state;
---
+++ src/effects/teleport.ts
@@ -0,0 +1,41 @@
+import Underworld from "../Underworld";
+import { HasSpace } from "../entity/Type";
+import { Vec2 } from "../jmath/Vec";
+import * as Unit from '../entity/Unit';
+import * as Pickup from '../entity/Pickup';
+import * as Obstacle from '../entity/Obstacle';
+import { skyBeam } from '../VisualEffects';
+import * as colors from '../graphics/ui/colors';
+
+export function teleport(object: HasSpace, newLocation: Vec2, underworld: Underworld, prediction: boolean, usePredictionLines?: boolean) {
+  if (usePredictionLines) {
+    // Show prediction lines before the move actually occurs
+    if (prediction && globalThis.predictionGraphics) {
+      globalThis.predictionGraphics.lineStyle(4, colors.forceMoveColor, 1.0);
+      globalThis.predictionGraphics.moveTo(object.x, object.y);
+      globalThis.predictionGraphics.lineTo(newLocation.x, newLocation.y);
+      // Draw circle at the end so the line path isn't a trail of rectangles with sharp edges
+      globalThis.predictionGraphics.lineStyle(1, colors.forceMoveColor, 1.0);
+      globalThis.predictionGraphics.beginFill(colors.forceMoveColor);
+      globalThis.predictionGraphics.drawCircle(newLocation.x, newLocation.y, 3);
+      globalThis.predictionGraphics.endFill();
+    }
+  }
+
+  if (Unit.isUnit(object)) {
+    // Moves the unit to location and resets path
+    Unit.setLocation(object, newLocation);
+    // Check to see if unit interacts with liquid
+    Obstacle.tryFallInOutOfLiquid(object, underworld, prediction);
+  } else if (Pickup.isPickup(object)) {
+    Pickup.setPosition(object, newLocation.x, newLocation.y);
+  } else {
+    object.x = newLocation.x;
+    object.y = newLocation.y;
+  }
+
+  if (!prediction) {
+    // Animate effect of unit spawning from the sky
+    skyBeam(newLocation);
+  }
+}
