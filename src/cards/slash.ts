e9f5780cf4741c35c29e697515472c7efa496773
Soul Muncher <68413161+SoulMuncher@users.noreply.github.com>
2024-01-25 20:26:22 -0600

    Fixed Slash Await (#410)
    * Fixed Slash Await
    
    * fix: Let slash animation finish
    
    even if there is a follow up spell.
    The removed line in this commit was
    added LONG ago to account for a spell
    (protect) which removed a targeting circle.
    I think this functionality is long obsolete
    but what it was currently doing was removing
    the slash animation (and others) before it was
    completed.
    
    Now animations that are added via a card effect
    will stick around until castCards has completed
    or until they remove themselves because they
    have completed animating.
    
    ---------
    
    Co-authored-by: Jordan O'Leary <jdoleary@gmail.com>

--- src/cards/slash.ts
+++ src/cards/slash.ts
@@ -32,56 +32,42 @@ const spell: Spell = {
     sfx: 'hurt',
     description: ['spell_slash', damageDone.toString()],
     effect: async (state, card, quantity, underworld, prediction) => {
-      let animationDelaySum = 0;
       // .filter: only target living units
       const targets = state.targetedUnits.filter(u => u.alive)
-      animationDelaySum = 0;
       let delayBetweenAnimations = delayBetweenAnimationsStart;
-      let attackPromises = [];
       // Note: quantity loop should always be INSIDE of the targetedUnits loop
       // so that any quantity-based animations will play simultaneously on multiple targets
       // but sequentially within themselves (on a single target, e.g. multiple hurts over and over)
       for (let q = 0; q < quantity; q++) {
         if (!prediction && !globalThis.headless) {
-          attackPromises.push(new Promise<void>((resolve) => {
-            setTimeout(() => {
-              playDefaultSpellSFX(card, prediction);
-              const slashAnimationPromises = targets.map(unit => {
-                return new Promise<void>(resolve => {
-                  const spellEffectImage = oneOffImage(unit, animationPath, containerSpells, resolve);
-                  if (spellEffectImage) {
-                    // Randomize rotation a bit so that subsequent slashes don't perfectly overlap
-                    spellEffectImage.sprite.rotation = randFloat(-Math.PI / 6, Math.PI / 6);
-                    if (q % 2 == 0) {
-                      // Flip every other slash animation so that it comes from the other side
-                      spellEffectImage.sprite.scale.x = -1;
-                    }
-                  }
-                  Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
-                })
-              })
-              Promise.all(slashAnimationPromises).then(() => {
-                resolve();
-              });
-
-            }, animationDelaySum);
-          }));
-          animationDelaySum += delayBetweenAnimations;
-          // Don't let it go below 20 milliseconds
-          delayBetweenAnimations = Math.max(20, delayBetweenAnimations);
+          playDefaultSpellSFX(card, prediction);
+          for (let unit of targets) {
+            const spellEffectImage = oneOffImage(unit, animationPath, containerSpells);
+            if (spellEffectImage) {
+              // Randomize rotation a bit so that subsequent slashes don't perfectly overlap
+              spellEffectImage.sprite.rotation = randFloat(-Math.PI / 6, Math.PI / 6);
+              if (q % 2 == 0) {
+                // Flip every other slash animation so that it comes from the other side
+                spellEffectImage.sprite.scale.x = -1;
+              }
+            }
+            Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
+          }
+          // Wait some delay between attacks
+          await new Promise(resolve => setTimeout(resolve, delayBetweenAnimations));
           // Juice: Speed up subsequent hits
           delayBetweenAnimations *= 0.80
+          // Don't let it go below 20 milliseconds
+          delayBetweenAnimations = Math.max(20, delayBetweenAnimations);
         } else {
           for (let unit of targets) {
             Unit.takeDamage(unit, damageDone, state.casterUnit, underworld, prediction, state);
           }
         }
       }
+
       if (targets.length == 0) {
         refundLastSpell(state, prediction, 'No valid targets. Cost refunded.');
-      }
-      if (!prediction && !globalThis.headless) {
-        await Promise.all(attackPromises);
       }
       return state;
     },
