b7fe5dc5a37f6641a109b0b4e32ba25de1f81163
Jamison Perkins <jamisonperkins@gmail.com>
2024-02-01 14:51:18 -0600

    Added Heal Mana Effect

--- src/cards/mana_steal.ts
+++ src/cards/mana_steal.ts
@@ -1,15 +1,10 @@
+import * as config from '../config';
 import { refundLastSpell, Spell } from './index';
-import floatingText from '../graphics/FloatingText';
-import { addPixiSpriteAnimated } from '../graphics/PixiUtils';
-import { manaBlue } from '../graphics/ui/colors';
-import { MultiColorReplaceFilter } from '@pixi/filter-multi-color-replace';
 import { makeManaTrail } from '../graphics/Particles';
 import { CardCategory } from '../types/commonTypes';
-import { playDefaultSpellSFX } from './cardUtils';
-import * as config from '../config';
-import { explain, EXPLAIN_OVERFILL } from '../graphics/Explain';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
 import { manaBurnCardId } from './mana_burn';
+import { healManaUnit } from '../effects/heal';
 
 const id = 'Mana Steal';
 const base_mana_stolen = 60;
@@ -44,62 +39,19 @@ const spell: Spell = {
           const unitManaStolen = Math.min(unit.mana, Math.ceil(remainingManaToSteal / (targets.length - i)));
           remainingManaToSteal -= unitManaStolen;
           unit.mana -= unitManaStolen;
-          const manaTrailPromises = [];
           if (!prediction) {
             for (let i = 0; i < quantity; i++) {
-              manaTrailPromises.push(makeManaTrail(unit, caster, underworld, '#e4f9ff', '#3fcbff'));
+              promises.push(makeManaTrail(unit, caster, underworld, '#e4f9ff', '#3fcbff'));
             }
           }
-          promises.push((prediction ? Promise.resolve() : Promise.all(manaTrailPromises)));
         }
       }
+      await Promise.all(promises)
+
       // In case there wasn't enough mana to steal
       totalManaStolen -= remainingManaToSteal;
-      await Promise.all(promises).then(() => {
-        state.casterUnit.mana += totalManaStolen;
-        if (!prediction) {
-          playDefaultSpellSFX(card, prediction);
-          // Animate
-          if (state.casterUnit.image) {
-            // Note: This uses the lower-level addPixiSpriteAnimated directly so that it can get a reference to the sprite
-            // and add a filter; however, addOneOffAnimation is the higher level and more common for adding a simple
-            // "one off" animated sprite.  Use it instead of addPixiSpriteAnimated unless you need more direct control like
-            // we do here
-            const animationSprite = addPixiSpriteAnimated('spell-effects/potionPickup', state.casterUnit.image.sprite, {
-              loop: false,
-              onComplete: () => {
-                if (animationSprite?.parent) {
-                  animationSprite.parent.removeChild(animationSprite);
-                }
-              }
-            });
-            if (animationSprite) {
-
-              if (!animationSprite.filters) {
-                animationSprite.filters = [];
-              }
-              // Change the health color to blue
-              animationSprite.filters.push(
-                new MultiColorReplaceFilter(
-                  [
-                    [0xff0000, manaBlue],
-                  ],
-                  0.1
-                )
-              );
-            }
-          }
-          explain(EXPLAIN_OVERFILL);
-        }
-      });
       if (totalManaStolen > 0) {
-        if (!prediction) {
-          floatingText({
-            coords: caster,
-            text: `+ ${totalManaStolen} Mana`,
-            style: { fill: 'blue', ...config.PIXI_TEXT_DROP_SHADOW }
-          });
-        }
+        await healManaUnit(state.casterUnit, totalManaStolen, underworld, prediction, state);
       } else {
         refundLastSpell(state, prediction, 'No targets have mana to steal\nHealth cost refunded')
       }
--- src/effects/heal.ts
+++ src/effects/heal.ts
@@ -3,15 +3,20 @@ import * as Unit from '../entity/Unit';
 import * as colors from '../graphics/ui/colors';
 import floatingText from "../graphics/FloatingText";
 import * as Image from '../graphics/Image';
+import * as config from '../config';
 import { playDefaultSpellSFX } from "../cards/cardUtils";
 import { healCardId } from "../cards/add_heal";
 import { EffectState } from "../cards";
+import { sendManaCardId } from "../cards/send_mana";
+import { EXPLAIN_OVERFILL, explain } from "../graphics/Explain";
 
-export async function healUnits(units: Unit.IUnit[], amount: number, underworld: Underworld, prediction: boolean, state?: EffectState, useHealFx: boolean = true) {
+const animationOptions = { loop: false, animationSpeed: 0.3 };
+
+export async function healUnits(units: Unit.IUnit[], amount: number, underworld: Underworld, prediction: boolean, state?: EffectState, useFx: boolean = true) {
   units = units.filter(u => u.alive);
-  if (units.length == 0) return;
+  if (units.length == 0 || amount == 0) return;
 
-  if (useHealFx && !prediction) {
+  if (useFx && !prediction) {
     if (allCards) {
       playDefaultSpellSFX(allCards[healCardId], prediction);
     }
@@ -21,7 +26,7 @@ export async function healUnits(units: Unit.IUnit[], amount: number, underworld:
       // Instead of using Promise.All()
       floatingText({ coords: unit, text: `+${Math.abs(amount)} Health` });
       Unit.takeDamage(unit, -amount, undefined, underworld, prediction, state);
-      animationPromise = Image.addOneOffAnimation(unit, 'spell-effects/potionPickup', {}, { loop: false, animationSpeed: 0.3 });
+      animationPromise = Image.addOneOffAnimation(unit, 'spell-effects/potionPickup', {}, animationOptions);
     }
     await animationPromise;
     return state;
@@ -33,7 +38,40 @@ export async function healUnits(units: Unit.IUnit[], amount: number, underworld:
   }
 }
 
-export async function healUnit(unit: Unit.IUnit, amount: number, underworld: Underworld, prediction: boolean, state?: EffectState, useHealFx: boolean = true) {
+export async function healUnit(unit: Unit.IUnit, amount: number, underworld: Underworld, prediction: boolean, state?: EffectState, useFx: boolean = true) {
   const units = [unit];
-  return await healUnits(units, amount, underworld, prediction, state, useHealFx);
+  return await healUnits(units, amount, underworld, prediction, state, useFx);
+}
+
+export async function healManaUnits(units: Unit.IUnit[], amount: number, underworld: Underworld, prediction: boolean, state?: EffectState, useFx: boolean = true) {
+  units = units.filter(u => u.alive);
+  if (units.length == 0 || amount == 0) return;
+
+  if (useFx && !prediction) {
+    if (allCards) {
+      playDefaultSpellSFX(allCards[sendManaCardId], prediction);
+    }
+    let animationPromise = undefined;
+    for (let unit of units) {
+      // All heals animate simultaneously, so just await the last promise
+      // Instead of using Promise.All()
+      floatingText({ coords: unit, text: `+ ${amount} Mana`, style: { fill: 'blue', ...config.PIXI_TEXT_DROP_SHADOW } });
+      unit.mana += amount;
+      explain(EXPLAIN_OVERFILL);
+      // TODO - Create mana version of this healing effect or improve support for colorfilter
+      animationPromise = Image.addOneOffAnimation(unit, 'spell-effects/potionPickup', {}, animationOptions);
+    }
+    await animationPromise;
+    return state;
+  } else {
+    for (let unit of units) {
+      unit.mana += amount;
+    }
+    return state;
+  }
+}
+
+export async function healManaUnit(unit: Unit.IUnit, amount: number, underworld: Underworld, prediction: boolean, state?: EffectState, useFx: boolean = true) {
+  const units = [unit];
+  return await healManaUnits(units, amount, underworld, prediction, state, useFx);
 }
