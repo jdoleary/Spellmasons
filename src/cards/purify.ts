0980d24cef5c725598be31b030aecdf1dcfc7342
Jordan O'Leary <jdoleary@gmail.com>
2024-02-18 07:39:57 -0500

    feat: Purify works on Cursed Mana Potions
    Closes: #507

--- src/cards/purify.ts
+++ src/cards/purify.ts
@@ -4,6 +4,8 @@ import Underworld from '../Underworld';
 import { CardCategory } from '../types/commonTypes';
 import { playDefaultSpellAnimation, playDefaultSpellSFX } from './cardUtils';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
+import * as Pickup from '../entity/Pickup';
+
 
 export const purifyCardId = 'purify';
 // Removes all curse modifiers
@@ -21,14 +23,32 @@ const spell: Spell = {
     description: 'spell_purify',
     effect: async (state, card, quantity, underworld, prediction) => {
       const targets = state.targetedUnits;
+      let doRefund = true;
       if (targets.length) {
+        doRefund = false;
         playDefaultSpellSFX(card, prediction);
         await playDefaultSpellAnimation(card, targets, prediction);
         for (let unit of targets) {
           apply(unit, underworld)
         }
       }
-      else {
+      if (state.targetedPickups.length) {
+        doRefund = false;
+        state.targetedPickups.filter(p => p.name === Pickup.CURSED_MANA_POTION).forEach(cursedManaPotion => {
+          const manaPotionSource = Pickup.pickups.find(p => p.name == Pickup.MANA_POTION);
+          if (manaPotionSource) {
+            Pickup.create({
+              pos: cursedManaPotion,
+              pickupSource: manaPotionSource,
+            }, underworld, prediction);
+          } else {
+            console.error('Could not find manaPotionSource')
+          }
+          Pickup.removePickup(cursedManaPotion, underworld, prediction);
+
+        })
+      }
+      if (doRefund) {
         refundLastSpell(state, prediction, 'No valid targets. Cost refunded.')
       }
       return state;
