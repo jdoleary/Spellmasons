42f744b1627db9a733eb3adb25d3b0ae3e854882
Jamison Perkins <jamisonperkins@gmail.com>
2024-02-24 19:15:25 -0600

    Improve Stomp FX Handling

--- src/cards/stomp.ts
+++ src/cards/stomp.ts
@@ -1,4 +1,4 @@
-import { getCurrentTargets, refundLastSpell, Spell } from './index';
+import { refundLastSpell, Spell } from './index';
 import { distance } from '../jmath/math';
 import { CardCategory } from '../types/commonTypes';
 import { playDefaultSpellSFX } from './cardUtils';
@@ -6,12 +6,13 @@ import * as config from '../config';
 import { drawUICirclePrediction } from '../graphics/PlanningView';
 import * as colors from '../graphics/ui/colors';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
-import { defaultPushDistance, forcePushAwayFrom, forcePushToDestination, forcePushTowards } from '../effects/force_move';
-import { baseExplosionRadius, explode } from '../effects/explode';
+import { forcePushAwayFrom, forcePushTowards } from '../effects/force_move';
 import { Vec2 } from '../jmath/Vec';
 import Underworld from '../Underworld';
-import { IUnit, playAnimation, takeDamage } from '../entity/Unit';
+import { IUnit, takeDamage } from '../entity/Unit';
 import { makeParticleExplosion } from '../graphics/ParticleCollection';
+import { createParticleTexture, logNoTextureWarning, simpleEmitter } from '../graphics/Particles';
+import * as particles from '@pixi/particle-emitter'
 
 export const stompCardId = 'stomp';
 const stompMoveDistance = 100;
@@ -38,28 +39,36 @@ const spell: Spell = {
         const moveDistance = Math.min(distance(state.casterUnit, target), stompMoveDistance * quantity)
         await forcePushTowards(state.casterUnit, target, moveDistance, underworld, prediction);
 
-        const delayBetweenStomps = 300; //ms
+        const delayBetweenStomps = 500; //ms
         const radius = stompRadius * (1 + (0.25 * state.aggregator.radiusBoost));
         for (let i = 1; i <= quantity; i++) {
-          if (!prediction && !globalThis.headless) {
-            // Await some delay between stomps, with extra delay before the final stomp
-            await new Promise(resolve => setTimeout(resolve, i == quantity ? delayBetweenStomps * 2 : delayBetweenStomps));
+          if (prediction) {
+            drawUICirclePrediction(state.casterUnit, radius, colors.errorRed, 'Stomp Radius');
+          } else if (!globalThis.headless) {
+            // Await some delay between stomps, then play stomp particles
+            await new Promise(resolve => setTimeout(resolve, delayBetweenStomps));
+            if (i < quantity) {
+              // Play stomp particles
+              makeStompExplodeParticles2(state.casterUnit, false, prediction);
+            } else {
+              // For final stomp, play implosion and then big stomp particles
+              makeStompWindupParticles(state.casterUnit, prediction);
+              await new Promise(resolve => setTimeout(resolve, delayBetweenStomps));
+              makeStompExplodeParticles2(state.casterUnit, true, prediction);
+            }
+            playSFXKey('bloatExplosion');
           }
 
           if (i < quantity) {
             // Early Stomp - does not push
-            stompExplode(state.casterUnit, radius, stompDamage, 0,
-              underworld, prediction,
-              colors.trueGrey, colors.trueWhite);
+            stompExplode(state.casterUnit, radius, stompDamage, 0, underworld, prediction);
           } else {
             // Final Stomp - Does pushback
-            stompExplode(state.casterUnit, radius, stompDamage, stompRadius,
-              underworld, prediction,
-              colors.trueGrey, colors.trueWhite);
+            stompExplode(state.casterUnit, radius, stompDamage, stompRadius, underworld, prediction);
           }
-
-          await underworld.awaitForceMoves();
         }
+
+        await underworld.awaitForceMoves();
       }
 
       return state;
@@ -67,26 +76,14 @@ const spell: Spell = {
   },
 };
 
-// Copied from explode.ts and modified slightly to
-// not damage caster, and not draw more prediction circles
-async function stompExplode(caster: IUnit, radius: number, damage: number, pushDistance: number, underworld: Underworld, prediction: boolean, colorstart?: number, colorEnd?: number) {
-  if (prediction) {
-    drawUICirclePrediction(caster, radius, colors.errorRed, 'Stomp Radius');
-  } else {
-    playSFXKey('bloatExplosion');
-    if (colorstart && colorEnd) {
-      makeParticleExplosion(caster, radius / baseExplosionRadius, colorstart, colorEnd, prediction);
-    }
-  }
-
+// Damages and pushes away nearby enemies
+async function stompExplode(caster: IUnit, radius: number, damage: number, pushDistance: number, underworld: Underworld, prediction: boolean) {
   const units = underworld.getUnitsWithinDistanceOfTarget(caster, radius, prediction).filter(u => u.id != caster.id);
 
-  if (damage != 0) {
-    units.forEach(u => {
-      // Deal damage to units
-      takeDamage(u, damage, u, underworld, prediction);
-    });
-  }
+  units.forEach(u => {
+    // Deal damage to units
+    takeDamage(u, damage, u, underworld, prediction);
+  });
 
   if (pushDistance > 0) {
     units.forEach(u => {
@@ -100,8 +97,157 @@ async function stompExplode(caster: IUnit, radius: number, damage: number, pushD
         forcePushAwayFrom(p, caster, pushDistance, underworld, prediction);
       })
   }
+}
 
-  return units;
+// Temporary particles for Stomp implementation
+function makeStompExplodeParticles2(position: Vec2, big: boolean, prediction: boolean) {
+  if (prediction || globalThis.headless) {
+    // Don't show if just a prediction
+    return;
+  }
+
+  makeParticleExplosion(position, big ? 0.8 : 0.5, colors.trueGrey, colors.trueWhite, prediction)
+}
+
+function makeStompExplodeParticles(position: Vec2, big: boolean, prediction: boolean) {
+  if (prediction || globalThis.headless) {
+    // Don't show if just a prediction
+    return;
+  }
+  const texture = createParticleTexture();
+  if (!texture) {
+    logNoTextureWarning('makeStompParticleExplosion');
+    return;
+  }
+  const config = {
+    "alpha": {
+      "start": 1,
+      "end": 0.1
+    },
+    "scale": {
+      "start": 2,
+      "end": 1.5,
+      "minimumScaleMultiplier": 0.01
+    },
+    "color": {
+      "start": "#634d42",
+      "end": "#26221f"
+    },
+    "speed": {
+      "start": 800,
+      "end": 0,
+      "minimumSpeedMultiplier": 0.5
+    },
+    "acceleration": {
+      "x": 0,
+      "y": 3000
+    },
+    "maxSpeed": 1000,
+    "startRotation": {
+      "min": -110,
+      "max": -70
+    },
+    "noRotation": false,
+    "rotationSpeed": {
+      "min": 0,
+      "max": 0
+    },
+    "lifetime": {
+      "min": 0.4,
+      "max": 0.8
+    },
+    "blendMode": "normal",
+    "frequency": 0.001,
+    "emitterLifetime": 0.1,
+    "maxParticles": 500,
+    "pos": {
+      "x": 0,
+      "y": 0
+    },
+    "addAtBack": false,
+    "spawnType": "circle",
+    "spawnCircle": {
+      "x": 0,
+      "y": 0,
+      "r": 80
+    }
+  };
+  if (big) {
+    config.scale.start *= 2;
+    config.scale.end *= 2;
+    config.speed.start *= 1.5;
+    config.lifetime.min *= 1.2;
+    config.lifetime.max *= 1.2;
+    config.spawnCircle.r *= 1.2;
+  }
+  simpleEmitter(position, particles.upgradeConfig(config, [texture]));
+}
+
+function makeStompWindupParticles(position: Vec2, prediction: boolean) {
+  if (prediction || globalThis.headless) {
+    // Don't show if just a prediction
+    return;
+  }
+  const texture = createParticleTexture();
+  if (!texture) {
+    logNoTextureWarning('makeStompParticleWindup');
+    return;
+  }
+  const config = {
+    "alpha": {
+      "start": 0.8,
+      "end": 0.1
+    },
+    "scale": {
+      "start": 1.5,
+      "end": 1,
+      "minimumScaleMultiplier": 0.01
+    },
+    "color": {
+      "start": "#634d42",
+      "end": "#26221f"
+    },
+    "speed": {
+      "start": 200,
+      "end": 0,
+      "minimumSpeedMultiplier": 0.5
+    },
+    "acceleration": {
+      "x": 0,
+      "y": 100
+    },
+    "maxSpeed": 0,
+    "startRotation": {
+      "min": 175,
+      "max": 185
+    },
+    "noRotation": false,
+    "rotationSpeed": {
+      "min": 0,
+      "max": 0
+    },
+    "lifetime": {
+      "min": 0.2,
+      "max": 0.4
+    },
+    "blendMode": "normal",
+    "frequency": 0.002,
+    "emitterLifetime": 0.5,
+    "maxParticles": 500,
+    "pos": {
+      "x": 0,
+      "y": 16
+    },
+    "addAtBack": false,
+    "spawnType": "ring",
+    "spawnCircle": {
+      "x": 0,
+      "y": 0,
+      "r": 80,
+      "minR": 32
+    }
+  };
+  simpleEmitter(position, particles.upgradeConfig(config, [texture]));
 }
 
 export default spell;
