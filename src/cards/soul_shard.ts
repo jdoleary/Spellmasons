cda470d05abdc49cccb4ca00c3bce3c31855be17
Jamison Perkins <jamisonperkins@gmail.com>
2024-03-12 12:33:21 -0500

    Soul Shard: Trail VFX

--- src/cards/soul_shard.ts
+++ src/cards/soul_shard.ts
@@ -1,4 +1,6 @@
 import * as Unit from '../entity/Unit';
+import * as color from '../graphics/ui/colors';
+import * as particles from '@pixi/particle-emitter'
 import { CardCategory } from '../types/commonTypes';
 import type Underworld from '../Underworld';
 import { oneOffImage, playDefaultSpellAnimation, playDefaultSpellSFX } from './cardUtils';
@@ -6,31 +8,42 @@ import { Spell, refundLastSpell } from './index';
 import { CardRarity, probabilityMap } from '../types/commonTypes';
 import { getOrInitModifier } from './util';
 import { distance } from '../jmath/math';
-import { makeManaTrail } from '../graphics/Particles';
+import { addTrail, calculateMaxParticles, createParticleTexture, makeManaTrail } from '../graphics/Particles';
 import { containerUnits, startBloodParticleSplatter } from '../graphics/PixiUtils';
+import { Vec2 } from '../jmath/Vec';
+import { addLerpable } from '../lerpList';
 
 const soulShardId = 'Soul Shard';
 const spell: Spell = {
   card: {
     id: soulShardId,
     category: CardCategory.Curses,
-    sfx: 'debilitate',
+    sfx: 'sacrifice',
     supportQuantity: true,
     manaCost: 60,
     healthCost: 0,
     expenseScaling: 3,
     probability: probabilityMap[CardRarity.RARE],
     thumbnail: 'spellIconSoulShard.png',
-    animationPath: 'spell-effects/spellDebilitate',
     description: ['spell_soul_shard'],
     effect: async (state, card, quantity, underworld, prediction) => {
       // .filter: only target living units
       const targets = state.targetedUnits.filter(u => u.alive && u.faction == state.casterUnit.faction);
       if (targets.length) {
         playDefaultSpellSFX(card, prediction);
-        await playDefaultSpellAnimation(card, targets, prediction);
+        unitTakeDamageFX(state.casterUnit, underworld, prediction);
+        // VFX Trails
+        if (!prediction) {
+          let promises = [];
+          for (let unit of targets) {
+            promises.push(makeSoulShardVFX(state.casterUnit, unit, underworld));
+          }
+          await Promise.all(promises);
+        }
+        // Effect
         for (let unit of targets) {
           Unit.addModifier(unit, soulShardId, underworld, prediction, quantity, { shardOwnerId: state.casterUnit.id });
+          unitTakeDamageFX(unit, underworld, prediction);
         }
       } else {
         refundLastSpell(state, prediction, "Must target an ally");
@@ -120,6 +133,80 @@ function add(unit: Unit.IUnit, _underworld: Underworld, _prediction: boolean, qu
 function unitById(id: number, underworld: Underworld, prediction: boolean): Unit.IUnit | undefined {
   const units = prediction ? underworld.unitsPrediction : underworld.units;
   return units.find(u => u.id == id);
+}
+
+function unitTakeDamageFX(unit: Unit.IUnit, underworld: Underworld, prediction: boolean) {
+  if (prediction) return;
+
+  playSFXKey(unit.sfx.damage);
+  Unit.playAnimation(unit, unit.animations.hit, { loop: false, animationSpeed: 0.2 });
+  //startBloodParticleSplatter(underworld, unit, unit, { maxRotationOffset: Math.PI / 16, numberOfParticles: 30 });
+  // Use all_red shader to flash the unit to show they are taking damage
+  if (unit.shaderUniforms.all_red) {
+    unit.shaderUniforms.all_red.alpha = 1;
+    addLerpable(unit.shaderUniforms.all_red, "alpha", 0, 200);
+  }
+}
+
+function makeSoulShardVFX(start: Vec2, target: Vec2, underworld: Underworld, totalNumberOfTrails?: number): Promise<void> {
+  const texture = createParticleTexture();
+  if (!texture) {
+    return Promise.resolve();
+  }
+  const { maxParticles, ratioToDefault } = calculateMaxParticles(90, totalNumberOfTrails);
+  return addTrail(
+    start,
+    target,
+    underworld,
+    particles.upgradeConfig({
+      autoUpdate: true,
+      alpha: {
+        start: 1,
+        end: 0
+      },
+      scale: {
+        start: 1,
+        end: 0.2,
+        minimumScaleMultiplier: 1
+      },
+      color: {
+        start: color.convertToHashColor(color.healthDarkRed),
+        end: color.convertToHashColor(color.healthDarkRed)
+      },
+      speed: {
+        start: 0,
+        end: 0,
+        minimumSpeedMultiplier: 1
+      },
+      acceleration: {
+        x: 0,
+        y: 0
+      },
+      maxSpeed: 0,
+      startRotation: {
+        min: 0,
+        max: 360
+      },
+      noRotation: true,
+      rotationSpeed: {
+        min: 0,
+        max: 0
+      },
+      lifetime: {
+        min: 0.4 * ratioToDefault,
+        max: 0.4 * ratioToDefault
+      },
+      blendMode: "normal",
+      frequency: 0.011,
+      emitterLifetime: -1,
+      maxParticles,
+      pos: {
+        x: 0,
+        y: 0
+      },
+      addAtBack: false,
+      spawnType: "point"
+    }, [texture]));
 }
 
 export default spell;
