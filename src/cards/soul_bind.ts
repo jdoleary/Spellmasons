675cdeeeb4eac7ee7cac7029d872c004876c0b01
Soul Muncher <68413161+SoulMuncher@users.noreply.github.com>
2024-05-07 16:29:15 -0500

    Soul Bind Visuals (#703)
    * Soul Bind: VFX draws for all soulbound units
    
    * Sould Bind: OnDrawSelected

--- src/cards/soul_bind.ts
+++ src/cards/soul_bind.ts
@@ -1,4 +1,5 @@
 import * as Unit from '../entity/Unit';
+import * as colors from '../graphics/ui/colors';
 import { CardCategory } from '../types/commonTypes';
 import type Underworld from '../Underworld';
 import { playDefaultSpellSFX } from './cardUtils';
@@ -9,6 +10,7 @@ import { jitter, lerpVec2 } from '../jmath/Vec';
 import { HasSpace } from '../entity/Type';
 
 const soulBindId = 'Soul Bind';
+const soulBindLineColor = 0x371f76;
 const spell: Spell = {
   card: {
     id: soulBindId,
@@ -26,12 +28,13 @@ const spell: Spell = {
       // .filter: only target living units
       const targets = state.targetedUnits.filter(u => u.alive);
       if (targets.length) {
-        playDefaultSpellSFX(card, prediction);
-        if (!prediction) {
-          await animate(targets);
-        }
         for (let unit of targets) {
           Unit.addModifier(unit, soulBindId, underworld, prediction, quantity);
+        }
+        if (!prediction) {
+          // Animate beam for all Soul Bound Units
+          playDefaultSpellSFX(card, prediction);
+          await animate(getSoulBoundUnits(underworld, prediction));
         }
       }
       return state;
@@ -43,9 +46,7 @@ const spell: Spell = {
   events: {
     onTakeDamage: (unit, amount, underworld, prediction) => {
       // Split the damage / healing amount among all soul bound units
-      let units = prediction ? underworld.unitsPrediction : underworld.units;
-      units = units.filter(u => !!u.modifiers[soulBindId] && u.alive && !u.flaggedForRemoval);
-
+      const units = getSoulBoundUnits(underworld, prediction);
       for (let i = 0; i < units.length; i++) {
         const unit = units[i];
         if (unit) {
@@ -66,15 +67,35 @@ const spell: Spell = {
 
       return 0;
     },
+    onDrawSelected: async (unit: Unit.IUnit, underworld: Underworld, prediction: boolean) => {
+      const modifier = unit.modifiers[soulBindId];
+      if (modifier) {
+        const soulBoundUnits = getSoulBoundUnits(underworld, prediction);
+        if (soulBoundUnits.length) {
+          const graphics = globalThis.selectedUnitGraphics;
+          if (graphics) {
+            for (let soulBoundUnit of soulBoundUnits) {
+              graphics.lineStyle(3, soulBindLineColor, 0.7);
+              graphics.moveTo(soulBoundUnit.x, soulBoundUnit.y);
+              graphics.lineTo(unit.x, unit.y);
+            }
+          }
+        }
+      }
+    }
   },
 };
 
 function add(unit: Unit.IUnit, _underworld: Underworld, _prediction: boolean, quantity: number = 1) {
   getOrInitModifier(unit, soulBindId, { isCurse: true, quantity }, () => {
     unit.onTakeDamageEvents.push(soulBindId);
+    unit.onDrawSelectedEvents.push(soulBindId);
   });
 }
 export default spell;
+function getSoulBoundUnits(underworld: Underworld, prediction: boolean): Unit.IUnit[] {
+  return (prediction ? underworld.unitsPrediction : underworld.units).filter(u => !!u.modifiers[soulBindId] && u.alive && !u.flaggedForRemoval);
+}
 async function animate(targets: HasSpace[]) {
   // Animations do not occur on headless
   if (!globalThis.headless) {
@@ -107,7 +128,7 @@ function drawLineBetweenTargets(targets: HasSpace[]): boolean {
         return false;
       }
       globalThis.predictionGraphics.clear();
-      globalThis.predictionGraphics.lineStyle(4, 0x371f76, 1.0)
+      globalThis.predictionGraphics.lineStyle(4, soulBindLineColor, 1.0);
       globalThis.predictionGraphics.moveTo(targets[0].x, targets[0].y);
       let from = targets[0];
       for (let target of targets) {
