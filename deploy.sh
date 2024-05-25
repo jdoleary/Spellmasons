b8a8c02843ff8def22a72e1d66d5fc444a90403e
Jordan O'Leary <jdoleary@gmail.com>
2024-05-25 12:42:26 -0400

    chore: Remove seashell from deploy
    Now that seashell has been deprovisioned

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,6 @@ docker build . -f Dockerfile.bun -t "$BETA_IMAGE_PATH:latest" -t "$BETA_IMAGE_PA
 docker push "$BETA_IMAGE_PATH:latest"
 docker push "$BETA_IMAGE_PATH:$PACKAGE_VERSION"
 
-SEASHELL_IMAGE_PATH=registry.digitalocean.com/jdoleary-containers/smms
-echo "Package Version:$PACKAGE_VERSION"
-docker build . -f Dockerfile.bun -t "$SEASHELL_IMAGE_PATH:latest" -t "$SEASHELL_IMAGE_PATH:$PACKAGE_VERSION"
-docker push "$SEASHELL_IMAGE_PATH:latest"
-docker push "$SEASHELL_IMAGE_PATH:$PACKAGE_VERSION"
-
 PUBLIC_IMAGE_PATH=jordanoleary/spellmasons-server
 # Now using @websocketpie/server-bun
 docker build . -f Dockerfile.bun -t "$PUBLIC_IMAGE_PATH:latest" -t "$PUBLIC_IMAGE_PATH:$PACKAGE_VERSION"
