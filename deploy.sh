6730884c9130562f319f5d3f2f0e9beb18296eb0
Jordan O'Leary <jdoleary@gmail.com>
2024-05-25 15:09:42 -0400

    chore: Add control plane commands to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,20 @@ PACKAGE_VERSION=$(cat package.json \
 
 # Build Dockerfile.bun for experimental transition from @websocketpie/server to @websocketpie/server-bun
 # docker build -f Dockerfile.bun . -t "spellmasons-bun:latest" -t "spellmasons-bun:$PACKAGE_VERSION"
-BETA_IMAGE_PATH=registry.digitalocean.com/jdoleary-containers/spellmasons-test
-echo "Package Version:$PACKAGE_VERSION"
-docker build . -f Dockerfile.bun -t "$BETA_IMAGE_PATH:latest" -t "$BETA_IMAGE_PATH:$PACKAGE_VERSION"
-docker push "$BETA_IMAGE_PATH:latest"
-docker push "$BETA_IMAGE_PATH:$PACKAGE_VERSION"
+# BETA_IMAGE_PATH=registry.digitalocean.com/jdoleary-containers/spellmasons-test
+# echo "Package Version:$PACKAGE_VERSION"
+# docker build . -f Dockerfile.bun -t "$BETA_IMAGE_PATH:latest" -t "$BETA_IMAGE_PATH:$PACKAGE_VERSION"
+# docker push "$BETA_IMAGE_PATH:latest"
+# docker push "$BETA_IMAGE_PATH:$PACKAGE_VERSION"
 
 PUBLIC_IMAGE_PATH=jordanoleary/spellmasons-server
 # Now using @websocketpie/server-bun
 docker build . -f Dockerfile.bun -t "$PUBLIC_IMAGE_PATH:latest" -t "$PUBLIC_IMAGE_PATH:$PACKAGE_VERSION"
 docker push "$PUBLIC_IMAGE_PATH:$PACKAGE_VERSION"
 docker push "$PUBLIC_IMAGE_PATH:latest"
-
 echo "Pushed image to Digital Ocean" 
 echo "Don't Forget to manually update any apps that are using the hub.docker.com image such as the Walrus server"
+
+# cpln image build --dockerfile Dockerfile.bun --push --name $PUBLIC_IMAGE_PATH:$PACKAGE_VERSION
+# cpln image build --dockerfile Dockerfile.bun --push --name $PUBLIC_IMAGE_PATH:latest
+# echo "Pushed to Control Plane
