ed0acb0dc1158cb2079abc53ba7864ce63ccb431
Soul Muncher <jamisonperkins@gmail.com>
2024-07-11 22:23:53 -0500

    Add Health Regen

--- src/cards/index.ts
+++ src/cards/index.ts
@@ -136,6 +136,7 @@ import registerPrimedCorpse from '../modifierPrimedCorpse';
 import registerSlime from '../modifierSlime';
 import registerTargetImmune, { targetImmuneId } from '../modifierTargetImmune';
 import registerGrowth from '../modifierGrowth';
+import registerHealthRegen from '../modifierHealthRegen';
 
 export interface Modifiers {
   subsprite?: Subsprite;
@@ -379,6 +380,8 @@ export function registerCards(overworld: Overworld) {
   registerUrnPoisonExplode();
   registerUrnExplosiveExplode();
   registerDeathmasonEvents();
+
+  registerHealthRegen();
 }
 
 // This is necessary because unit stats change with difficulty.
---
+++ src/modifierHealthRegen.ts
@@ -0,0 +1,36 @@
+import { registerEvents, registerModifiers } from "./cards";
+import { getOrInitModifier } from "./cards/util";
+import { healUnit } from "./effects/heal";
+import * as Unit from './entity/Unit';
+import Underworld from './Underworld';
+
+export const healthRegenId = 'Health Regen';
+export default function registerHealthRegen() {
+  registerModifiers(healthRegenId, {
+    add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
+      getOrInitModifier(unit, healthRegenId, { isCurse: false, quantity, keepOnDeath: true }, () => {
+        Unit.addEvent(unit, healthRegenId);
+      });
+
+      if (!prediction) {
+        updateTooltip(unit);
+      }
+    }
+  });
+  registerEvents(healthRegenId, {
+    onTurnStart: async (unit: Unit.IUnit, underworld: Underworld, prediction: boolean) => {
+      const modifier = unit.modifiers[healthRegenId];
+      if (modifier) {
+        healUnit(unit, modifier.quantity, unit, underworld, prediction);
+      }
+    }
+  });
+}
+
+function updateTooltip(unit: Unit.IUnit) {
+  const modifier = unit.modifiers[healthRegenId];
+  if (modifier) {
+    // Set tooltip:
+    modifier.tooltip = `${modifier.quantity} ${i18n('Health')} ${i18n('Regen')}`
+  }
+}
