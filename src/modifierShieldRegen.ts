f6d802a69b332ea210204b451f63aadfe6e6531d
Soul Muncher <jamisonperkins@gmail.com>
2024-07-11 22:20:00 -0500

    Add Shield Regen

---
+++ src/modifierShieldRegen.ts
@@ -0,0 +1,36 @@
+import { registerEvents, registerModifiers } from "./cards";
+import { shieldId } from "./cards/shield";
+import { getOrInitModifier } from "./cards/util";
+import * as Unit from './entity/Unit';
+import Underworld from './Underworld';
+
+export const shieldRegenId = 'Shield Regen';
+export default function registerShieldRegen() {
+  registerModifiers(shieldRegenId, {
+    add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
+      getOrInitModifier(unit, shieldRegenId, { isCurse: false, quantity, keepOnDeath: true }, () => {
+        Unit.addEvent(unit, shieldRegenId);
+      });
+
+      if (!prediction) {
+        updateTooltip(unit);
+      }
+    }
+  });
+  registerEvents(shieldRegenId, {
+    onTurnStart: async (unit: Unit.IUnit, underworld: Underworld, prediction: boolean) => {
+      const modifier = unit.modifiers[shieldRegenId];
+      if (modifier) {
+        Unit.addModifier(unit, shieldId, underworld, prediction, modifier.quantity);
+      }
+    }
+  });
+}
+
+function updateTooltip(unit: Unit.IUnit) {
+  const modifier = unit.modifiers[shieldRegenId];
+  if (modifier) {
+    // Set tooltip:
+    modifier.tooltip = `${modifier.quantity} ${i18n('Shield')} ${i18n('Regen')}`
+  }
+}
