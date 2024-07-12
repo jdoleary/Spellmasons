ad93d99a61a89b3888a6a2450d23539eeb095f69
Soul Muncher <jamisonperkins@gmail.com>
2024-07-11 22:30:03 -0500

    Fix Debilitate Quantity
    - Previously stacked [2x, 4x, 6x, 8x,...] damage, meaning the first one would give you an additive +100% boost, then each one after would give an additive +200% boost
    - Made description more clear
    - Now allows any modifier percentage (as specified by quantity)

--- src/cards/debilitate.ts
+++ src/cards/debilitate.ts
@@ -9,7 +9,7 @@ import { getOrInitModifier } from './util';
 
 const id = 'Debilitate';
 const imageName = 'spellIconDebilitate.png';
-const proportion = 2;
+const percentDamageIncrease = 100;
 const spell: Spell = {
   card: {
     id,
@@ -22,7 +22,7 @@ const spell: Spell = {
     probability: probabilityMap[CardRarity.UNCOMMON],
     thumbnail: 'spellIconDebilitate.png',
     animationPath: 'spell-effects/spellDebilitate',
-    description: ['spell_debilitate', (proportion * 100).toString()],
+    description: ['spell_debilitate', percentDamageIncrease.toString()],
     effect: async (state, card, quantity, underworld, prediction) => {
       // .filter: only target living units
       const targets = state.targetedUnits.filter(u => u.alive);
@@ -30,7 +30,7 @@ const spell: Spell = {
         playDefaultSpellSFX(card, prediction);
         await playDefaultSpellAnimation(card, targets, prediction);
         for (let unit of targets) {
-          Unit.addModifier(unit, id, underworld, prediction, quantity);
+          Unit.addModifier(unit, id, underworld, prediction, 100);
         }
       }
       return state;
@@ -41,14 +41,16 @@ const spell: Spell = {
   },
   events: {
     onTakeDamage: (unit, amount, _underworld, damageDealer) => {
-      const quantity = unit.modifiers[id]?.quantity || 1;
-      // Magnify positive damage
-      if (amount > 0) {
-        return amount * proportion * quantity;
-      } else {
-        // Do not magnify negative damage (which is healing)
-        return amount;
+      const modifier = unit.modifiers[id];
+      if (modifier) {
+        // Will only increase damage (doesn't affect incoming healing)
+        if (amount > 0) {
+          // Each quantity = 1% damage boost
+          amount = Math.floor(amount * CalcMult(modifier.quantity));
+        }
       }
+
+      return amount;
     },
   },
 };
@@ -57,5 +59,20 @@ function add(unit: Unit.IUnit, _underworld: Underworld, _prediction: boolean, qu
   getOrInitModifier(unit, id, { isCurse: true, quantity }, () => {
     Unit.addEvent(unit, id);
   });
+
+  updateTooltip(unit);
+}
+
+function updateTooltip(unit: Unit.IUnit) {
+  const modifier = unit.modifiers[id];
+  if (modifier) {
+    // Set tooltip:
+    modifier.tooltip = `${CalcMult(modifier.quantity)}x ${i18n('Incoming')} ${i18n('Damage')}`;
+  }
+}
+
+function CalcMult(quantity: number): number {
+  // Each Quantity = 1% damage boost
+  return 1 + (quantity / 100);
 }
 export default spell;
