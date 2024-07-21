877b56d77d744adaac230710797138e013553d73
Soul Muncher <jamisonperkins@gmail.com>
2024-07-21 14:41:27 -0500

    Fix Thorns Infinite Loop

--- src/modifierThorns.ts
+++ src/modifierThorns.ts
@@ -24,12 +24,22 @@ export default function registerThorns() {
       if (modifier) {
         // Thorns will not deal damage if we are being healed
         if (damageDealer && amount > 0) {
+          // Thorns should not trigger thorns
+          // Exception: Temporarily remove the damageDealer's Thorns event to prevent infinite loop
+          const index = damageDealer.events.findIndex(e => e == thornsId);
+          damageDealer.events.splice(index, 1);
+
           // Deal flat damage to the attacker
           Unit.takeDamage({
             unit: damageDealer,
             amount: modifier.quantity,
             sourceUnit: unit,
           }, underworld, prediction);
+
+          // Restore the Thorns event if the damageDealer still has the Thorns modifier
+          if (damageDealer.modifiers[thornsId]) {
+            Unit.addEvent(damageDealer, thornsId);
+          }
         }
       }
 
