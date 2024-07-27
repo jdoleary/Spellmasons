0793d5b3035e4196f71813917a6bd83a30b41eb8
Jordan O'Leary <jdoleary@gmail.com>
2024-07-27 16:43:27 -0400

    vfx: Growth and Slime

--- src/modifierGrowth.ts
+++ src/modifierGrowth.ts
@@ -6,33 +6,32 @@ import Underworld from './Underworld';
 import floatingText from "./graphics/FloatingText";
 
 export const growthId = 'Growth';
-// TODO EFFECT
-// const subspriteId = 'spell-effects/growth';
-// function addModifierVisuals(unit: Unit.IUnit, underworld: Underworld) {
-//   Image.addSubSprite(unit.image, subspriteId);
-// }
+const subspriteId = 'spell-effects/growth';
+function addModifierVisuals(unit: Unit.IUnit, underworld: Underworld) {
+  Image.addSubSprite(unit.image, subspriteId);
+}
 export default function registerGrowth() {
   registerModifiers(growthId, {
     description: 'Unit gets stronger every turn.',
     probability: 100,
-    // addModifierVisuals,
+    addModifierVisuals,
     add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
       getOrInitModifier(unit, growthId, { isCurse: false, quantity, keepOnDeath: true }, () => {
         Unit.addEvent(unit, growthId);
       });
     },
-    // subsprite: {
-    //   imageName: subspriteId,
-    //   alpha: 1.0,
-    //   anchor: {
-    //     x: 0.5,
-    //     y: 0.5,
-    //   },
-    //   scale: {
-    //     x: 1.0,
-    //     y: 1.2,
-    //   },
-    // },
+    subsprite: {
+      imageName: subspriteId,
+      alpha: 1.0,
+      anchor: {
+        x: 0.7,
+        y: 0.5,
+      },
+      scale: {
+        x: 0.8,
+        y: 0.8,
+      },
+    },
   });
 
   registerEvents(growthId, {
--- src/modifierSlime.ts
+++ src/modifierSlime.ts
@@ -1,19 +1,37 @@
 import { registerEvents, registerModifiers } from "./cards";
 import { animateMitosis, doCloneUnit } from "./cards/clone";
 import { getOrInitModifier } from "./cards/util";
+import * as Image from './graphics/Image';
 import * as Unit from './entity/Unit';
 import floatingText from "./graphics/FloatingText";
 import Underworld from './Underworld';
 
 export const slimeId = 'Slime';
+const subspriteId = 'spell-effects/slime';
+function addModifierVisuals(unit: Unit.IUnit, underworld: Underworld) {
+  Image.addSubSprite(unit.image, subspriteId);
+}
 export default function registerSlime() {
   registerModifiers(slimeId, {
     description: 'Causes a unit to split into two units every turn.',
     probability: 100,
+    addModifierVisuals,
     add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
       getOrInitModifier(unit, slimeId, { isCurse: false, quantity, keepOnDeath: true }, () => {
         Unit.addEvent(unit, slimeId);
       });
+    },
+    subsprite: {
+      imageName: subspriteId,
+      alpha: 1.0,
+      anchor: {
+        x: 0.5,
+        y: 0,
+      },
+      scale: {
+        x: 0.6,
+        y: 0.6,
+      },
     },
   });
   registerEvents(slimeId, {
