93f264c40b57c123c05dec1c7b87d7a5009cfe02
Soul Muncher <jamisonperkins@gmail.com>
2024-08-10 15:21:49 -0500

    Balance: Potion barrier no longer scales with pickup power

--- src/modifierPotionBarrier.ts
+++ src/modifierPotionBarrier.ts
@@ -24,7 +24,7 @@ export default function registerPotionBarrier() {
       const modifier = unit.modifiers[potionBarrierId]
       if (modifier) {
         if (pickup.name.includes("Potion")) {
-          const shieldToAdd = Math.floor(modifier.quantity * pickup.power);
+          const shieldToAdd = Math.floor(modifier.quantity);
           if (shieldToAdd > 0) {
             Unit.addModifier(unit, shieldId, underworld, prediction, shieldToAdd);
           }
