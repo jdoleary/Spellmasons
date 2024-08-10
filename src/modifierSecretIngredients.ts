b55e88ddf377c15ef82b7ce0d05f9e8d46b3ed02
Soul Muncher <jamisonperkins@gmail.com>
2024-08-09 19:10:27 -0500

    SecretIngredients: Affects all potions in range

--- src/modifierSecretIngredients.ts
+++ src/modifierSecretIngredients.ts
@@ -1,5 +1,5 @@
 import { registerEvents, registerModifiers } from "./cards";
-import { getOrInitModifier, Modifier } from "./cards/util";
+import { getOrInitModifier } from "./cards/util";
 import * as Unit from './entity/Unit';
 import Underworld from './Underworld';
 import { distance } from "./jmath/math";
@@ -7,15 +7,13 @@ import { setPower } from "./entity/Pickup";
 import { makeManaTrail } from "./graphics/Particles";
 import { getColorFromPotion } from "./cards/potion_shatter";
 import { convertToHashColor } from "./graphics/ui/colors";
-import { chooseOneOfSeeded, getUniqueSeedString } from "./jmath/rand";
-import seedrandom from "seedrandom";
 
-// Empower [quantity] potions within cast range each turn. (Can target the same potion multiple times)
+// Empower potions within cast range each turn
 export const secretIngredientsId = 'Secret Ingredients';
 export default function registerSecretIngredients() {
   registerModifiers(secretIngredientsId, {
     description: 'rune_secret_ingredients',
-    costPerUpgrade: 100,
+    costPerUpgrade: 200,
     add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
       getOrInitModifier(unit, secretIngredientsId, { isCurse: false, quantity, keepOnDeath: true }, () => {
         Unit.addEvent(unit, secretIngredientsId);
@@ -26,20 +24,12 @@ export default function registerSecretIngredients() {
     onTurnStart: async (unit: Unit.IUnit, underworld: Underworld, prediction: boolean) => {
       const modifier = unit.modifiers[secretIngredientsId];
       if (modifier && unit.alive) {
-        const random = seedrandom(`${getUniqueSeedString(underworld)} - ${unit.id}`);
         const potions = getAllPotionsInAttackRange(unit, underworld, prediction);
-        if (potions.length > 0) {
-          for (let i = 0; i < modifier.quantity; i++) {
-            const potion = chooseOneOfSeeded(potions, random);
-            if (potion) {
-              makeManaTrail(unit, potion, underworld, convertToHashColor(getColorFromPotion(potion)), '#ff0000', potions.length * modifier.quantity).then(() =>
-                setPower(potion, potion.power + modifier.quantity)
-              );
-            } else {
-              console.error("Could not select potion from potions: ", potions);
-            }
-          }
-        }
+        potions.forEach(p => {
+          makeManaTrail(unit, p, underworld, convertToHashColor(getColorFromPotion(p)), '#ff0000', potions.length * modifier.quantity).then(() =>
+            setPower(p, p.power + modifier.quantity)
+          );
+        });
       }
     }
   });
