09fe1b44989bbc39781242b77a0ce633915657bd
Jordan O'Leary <jdoleary@gmail.com>
2024-08-14 09:41:19 -0400

    fix: Swapping with pickups no longer
    picks up the pickup.
    
    Thanks Liese!

--- src/cards/swap.ts
+++ src/cards/swap.ts
@@ -32,18 +32,29 @@ const spell: Spell = {
       // Swap the casterUnit with the last target
       const casterSwapTarget = targets[targets.length - 1];
       if (casterSwapTarget) {
-        swaps.push([casterUnit, casterSwapTarget]);
+        swaps.push([casterUnit, Vec.clone(casterSwapTarget)]);
       }
       const swapLocations = [swapLocation, ...underworld.findValidSpawns({ spawnSource: swapLocation, ringLimit: 4, prediction, radius: config.COLLISION_MESH_RADIUS / 4 })];
 
       for (let targetObject of targets) {
         if (targetObject) {
-          swaps.push([targetObject, swapLocations.shift() || swapLocation]);
+          swaps.push([targetObject, Vec.clone(swapLocations.shift() || swapLocation)]);
         }
       }
 
+      // Swap pickups first.  This prevents pickups from being "picked up" when swapped with a unit
+      // because unit setLocation triggers a check pickup collision but pickup setLocation does not
+      // therefore if the pickups swap first, the pickup will not be where the unit arrives to
+      // pick it up
       for (let [object, newLocation] of swaps) {
-        teleport(object, newLocation, underworld, prediction);
+        if (Pickup.isPickup(object)) {
+          teleport(object, newLocation, underworld, prediction);
+        }
+      }
+      for (let [object, newLocation] of swaps) {
+        if (Unit.isUnit(object)) {
+          teleport(object, newLocation, underworld, prediction);
+        }
       }
       return state;
     },
