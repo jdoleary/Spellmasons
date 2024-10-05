0e27ac2c0ff7e6a7a58cc3d1ed0cc6c3e5358b71
Jordan O'Leary <jdoleary@gmail.com>
2024-10-05 16:38:41 -0400

    optim: Throttle skybeam
    This massively increases efficiency for when lots of enemies are teleported to the same place all at once.
    
    Hugely reduces lag.

--- src/VisualEffects.ts
+++ src/VisualEffects.ts
@@ -1,11 +1,27 @@
 import { COLLISION_MESH_RADIUS } from "./config";
 import { makeLightBeamParticles } from "./graphics/ParticleCollection";
 import { addPixiSprite, containerProjectiles, containerSpells, containerUnits } from "./graphics/PixiUtils";
-import { Vec2 } from "./jmath/Vec";
+import { equal, Vec2 } from "./jmath/Vec";
 
 // A 'level up' style beam that is cast down from the sky and fades out on it's own
+let recentSkybeams: { position: Vec2, time: number }[] = []
+const THROTTLE_TIME = 500;
 export function skyBeam(position: Vec2) {
-    // Note: T his is in containerProjectiles instead of containerSpells
+    // For whatever reason, many skybeams is expensive,
+    // so if multiple are triggered in the exact same position,
+    // throttle them.
+    const now = Date.now();
+    // 500: within 500ms
+    if (recentSkybeams.filter(r => equal(r.position, position) && (now - r.time) <= THROTTLE_TIME).length > 2) {
+        return;
+    }
+    // Add this skybeam to the list
+    recentSkybeams.push({ position, time: now });
+    // Clear skybeam records that are beyond THROTTLE_TIME
+    recentSkybeams = recentSkybeams.filter(x => (now - x.time) <= THROTTLE_TIME);
+
+
+    // Note: This is in containerProjectiles instead of containerSpells
     // because containerSpells automatically clears it's contents while
     // containerProjectiles does not.  containerProjectiles's children
     // are expected to clear themselves, which is what the skybeam does
