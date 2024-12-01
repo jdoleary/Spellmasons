7240084165f46d2e91b07dbccdb7cd8c413f0f65
Jordan O'Leary <jdoleary@gmail.com>
2024-11-30 22:00:23 -0500

    balance: Make reroll runes cheaper

--- src/modifierReroll.ts
+++ src/modifierReroll.ts
@@ -7,7 +7,7 @@ const rerollRuneId = 'reroll'
 export default function registerStatUpgradeModifiers() {
   registerModifiers(rerollRuneId, {
     description: `reroll_rune_description`,
-    _costPerUpgrade: 20,
+    _costPerUpgrade: 15,
     constant: true,
     add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
       const player = underworld.players.find(p => p.unit == unit);
