909964280ee23debe3b37266f24dde23c56d7c77
Jordan O'Leary <jdoleary@gmail.com>
2024-12-18 12:48:48 -0500

    balance: Cost of investment

--- src/modifierInvestment.ts
+++ src/modifierInvestment.ts
@@ -1,16 +1,15 @@
-import { registerEvents, registerModifiers } from "./cards";
+import { registerModifiers } from "./cards";
 import { getOrInitModifier } from "./cards/util";
 import * as Unit from './entity/Unit';
 import Underworld from './Underworld';
-import floatingText from "./graphics/FloatingText";
 
-// Gain 10% of stored SP every level
+// Gain 5% of stored SP every level
 export const investmentId = 'Investment';
 export default function registerInvestment() {
   registerModifiers(investmentId, {
     description: ['rune_investment'],
     unitOfMeasure: '%',
-    _costPerUpgrade: 50,
+    _costPerUpgrade: 30,
     quantityPerUpgrade: 5,
     maxUpgradeCount: 5,
     add: (unit: Unit.IUnit, underworld: Underworld, prediction: boolean, quantity: number = 1) => {
