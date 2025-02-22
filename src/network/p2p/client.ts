96493fbf2ef3cea2cadab6a7c6d400e3e6d19800
Jordan O'Leary <jdoleary@gmail.com>
2025-02-22 16:06:42 -0500

    fix: Allow client to join another peer after
    disconnecting.

--- src/network/p2p/client.ts
+++ src/network/p2p/client.ts
@@ -11,6 +11,10 @@ import { ACCEPT_REQUEST_SIGNAL, ERROR, JOIN_REQUEST, REQUEST_REJECTED } from "./
 5. Client recieve signal and form connection
 */
 
+const handlers: { onData: (data: any, socket: WebSocket) => void, onError: (data: any, socket: WebSocket) => void } = {
+    onData: () => console.error('No onData handler set up'),
+    onError: () => console.error('No onError handler set up')
+};
 // This function is exposed for the consumer of this library 
 // to initiate a join request to a host for a p2p connection
 export async function join({ toName, fromName, fromClientId, websocketHubUrl, onError, onData, onPeerDisconnected }: { toName: string, fromName: string, fromClientId: string, websocketHubUrl: string, onError: (error: any) => void, onData: (data: any) => void, onPeerDisconnected: (p: SimplePeer) => void }): Promise<{ peer: SimplePeer, name: string }> {
@@ -77,8 +81,16 @@ export async function join({ toName, fromName, fromClientId, websocketHubUrl, on
             }
         }
 
+        // Important: we must use the same `handlers` object
+        // and just reassign the properties so that if the 
+        // socket already exists, it will invoke the new
+        // callbacks instead of the old ones that have
+        // closure'd over the old peer connection
+        handlers.onData = onHubData;
+        handlers.onError = onError;
+
         // Step 2: Connect to hub
-        const socket = await ensureConnectionToHub(websocketHubUrl, { onData: onHubData, onError });
+        const socket = await ensureConnectionToHub(websocketHubUrl, handlers);
         console.log('Step: Connected to hub and sending join request with signal.');
         // Step 3: Now that we're connected to hub and have a signal,
         // send a join request with the signal to the host through
