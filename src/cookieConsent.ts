5447d98ac85c0a30974fd130ce6632aed6598cca
Jordan O'Leary <jdoleary@gmail.com>
2025-08-03 16:41:00 -0400

    chore: Update privacy policy and EULA

--- src/cookieConsent.ts
+++ src/cookieConsent.ts
@@ -1,5 +1,5 @@
 import * as storage from './storage';
-const PRIVACY_POLICY_AND_EULA_CONSENT_STORAGE_KEY = 'privacy_policy_and_eula_consent_nov_2025';
+const PRIVACY_POLICY_AND_EULA_CONSENT_STORAGE_KEY = 'privacy_policy_and_eula_consent_august_2025';
 const PRIVACY_POLICY_AND_EULA_CONSENT_STORAGE_VALUE = 'accepted';
 export function areCookiesAllowed() {
     return localStorage.getItem(PRIVACY_POLICY_AND_EULA_CONSENT_STORAGE_KEY) === PRIVACY_POLICY_AND_EULA_CONSENT_STORAGE_VALUE;
